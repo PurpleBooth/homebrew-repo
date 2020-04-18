f9cada666d652ff9562f3de9eda55e9cb7810535
Billie Thompson <billie@billiecodes.com>
2020-04-18 21:16:30 +0200

    Added zsh
    This adds zsh as a dependency.

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -6,6 +6,7 @@ class FixCompaudit < Formula
   sha256 "10d138f06eb66de20f8301b928dd3955e6e16716161122eaeb6df2c9b3c7651f"
 
   depends_on "rust" => :build
+  depends_on "zsh"
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
