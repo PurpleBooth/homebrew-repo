7c05064708aa94c12ccc9fdecfd99c1535bb64fd
Billie Thompson <billie@billiecodes.com>
2020-04-19 11:54:50 +0200

    Add art station
    This adds art station i guess the wallpaper downloader

---
+++ Formula/art-station-i-guess.rb
@@ -0,0 +1,19 @@
+class ArtStationIGuess < Formula
+  desc "Download art from Art Station for wallpaper"
+  homepage "https://github.com/PurpleBooth/art-station-i-guess"
+  url "https://github.com/PurpleBooth/art-station-i-guess/archive/v0.0.0.tar.gz"
+  version "0.0.0"
+  sha256 "tbd"
+
+  depends_on "rust" => :build
+
+  def install
+        ENV['VERSION'] = version
+        system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/art-station-i-guess", "-h"
+    system "#{bin}/art-station-i-guess", "-V"
+  end
+end
