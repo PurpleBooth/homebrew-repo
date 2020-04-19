5af3c04485de283c0328ea75955060eb49456b9e
Billie Thompson <billie@billiecodes.com>
2020-04-19 07:46:41 +0200

    Add game of life
    This adds the game of life as an installable package

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -1,12 +1,11 @@
-class FixCompaudit < Formula
+class GameOfLife < Formula
   desc "Fixes problems reported by compuaudit"
-  homepage "https://github.com/PurpleBooth/fix-compaudit"
-  url "https://github.com/PurpleBooth/fix-compaudit/archive/v0.3.2.tar.gz"
-  version "0.3.2"
-  sha256 "c3971e6520e3bbda868be2f2d2cd8f45925dc16cec8d0eceec2dfd95eb454bca"
+  homepage "https://github.com/PurpleBooth/game-of-life"
+  url "https://github.com/PurpleBooth/game-of-life/archive/v0.0.0.tar.gz"
+  version "0.0.0"
+  sha256 "tbd"
 
   depends_on "rust" => :build
-  depends_on "zsh"
 
   def install
         ENV['VERSION'] = version
@@ -14,7 +13,7 @@ class FixCompaudit < Formula
   end
 
   test do
-    system "#{bin}/fix-compaudit", "-h"
-    system "#{bin}/fix-compaudit", "-V"
+    system "#{bin}/game-of-life", "-h"
+    system "#{bin}/game-of-life", "-V"
   end
 end
