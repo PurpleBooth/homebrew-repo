8a1118ed6b19fafea14d5ff485fc0a13dfb0d0b3
Billie Thompson <billie@billiecodes.com>
2020-04-19 18:19:33 +0200

    Add keyring creator thing

---
+++ Formula/setup-a-keyring-for-sops.rb
@@ -0,0 +1,19 @@
+class SetupAKeyringForSops < Formula
+  desc "Use gcloud to setup everything needed for SOPS"
+  homepage "https://github.com/PurpleBooth/setup-a-keyring-for-sops"
+  url "https://github.com/PurpleBooth/art-station-i-guess/archive/v0.1.2.tar.gz"
+  version "0.1.2"
+  sha256 "82545577ae01c2b3d2031b7d7d473e38c0cf8374c40e7b6e66ec1169b98900e8"
+
+  depends_on "rust" => :build
+
+  def install
+        ENV['VERSION'] = version
+        system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/setup-a-keyring-for-sops", "-h"
+    system "#{bin}/setup-a-keyring-for-sops", "-V"
+  end
+end
