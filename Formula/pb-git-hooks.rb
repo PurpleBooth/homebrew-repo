422bf8ee3a2b8a2ec585b99cb61ef979126d6d2b
Billie Thompson <billie@billiecodes.com>
2020-04-21 20:22:10 +0200

    Added a formula for my git hooks

---
+++ Formula/pb-git-hooks.rb
@@ -0,0 +1,21 @@
+class PbGitHooks < Formula
+  desc "My personal git commit hooks"
+  homepage "https://github.com/PurpleBooth/pb-git-hooks"
+  url "https://github.com/PurpleBooth/pb-git-hooks/archive/v0.0.0.tar.gz"
+  version "0.0.0"
+  sha256 "na"
+
+  depends_on "rust" => :build
+
+  def install
+        ENV['VERSION'] = version
+        system "cargo", "install", "--locked", "--root", prefix, "--path", "./pb-commit-msg/"
+        system "cargo", "install", "--locked", "--root", prefix, "--path", "./pb-pre-commit/"
+        system "cargo", "install", "--locked", "--root", prefix, "--path", "./pb-prepare-commit-msg/"
+  end
+
+  test do
+    system "#{bin}/art-station-i-guess", "-h"
+    system "#{bin}/art-station-i-guess", "-V"
+  end
+end
