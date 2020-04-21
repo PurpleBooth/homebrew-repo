8d95c2895e4cbb878c8de3fcf4579d738da945ee
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-21 20:09:52 +0200

    setup-a-keyring-for-sops 0.5.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/setup-a-keyring-for-sops.rb
+++ Formula/setup-a-keyring-for-sops.rb
@@ -13,9 +13,9 @@ end
 class SetupAKeyringForSops < Formula
   desc "Use gcloud to setup everything needed for SOPS"
   homepage "https://github.com/PurpleBooth/setup-a-keyring-for-sops"
-  url "https://github.com/PurpleBooth/setup-a-keyring-for-sops/archive/v0.4.0.tar.gz"
-  version "0.4.0"
-  sha256 "fb84b88e0b4ec4418bda0d2f3da04c42c70b84f10b5ae897fbe1a77243ac0c18"
+  url "https://github.com/PurpleBooth/setup-a-keyring-for-sops/archive/v0.5.0.tar.gz"
+  version "0.5.0"
+  sha256 "cd7d22f8be897d73140516aa37465aa251254dca5b9674cf43d1a8715c0f595e"
 
   depends_on "rust" => :build
   depends_on GCloudRequirement
