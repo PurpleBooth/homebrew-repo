d735f75e2b4df51f159af4f8ec8d4531aa1bdcca
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-21 19:44:49 +0200

    fix-compaudit 0.6.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -1,9 +1,9 @@
 class FixCompaudit < Formula
   desc "Fixes problems reported by compuaudit"
   homepage "https://github.com/PurpleBooth/fix-compaudit"
-  url "https://github.com/PurpleBooth/fix-compaudit/archive/v0.5.0.tar.gz"
-  version "0.5.0"
-  sha256 "76d32cbc2d1317b2607a7eec92872ca50e740ee99d4468fda2757ccad4d58593"
+  url "https://github.com/PurpleBooth/fix-compaudit/archive/v0.6.0.tar.gz"
+  version "0.6.0"
+  sha256 "5166250259aca62c784cc139d11a2057dfa575b2758038f5c4c58df46530fa89"
 
   depends_on "rust" => :build
   depends_on "zsh"
