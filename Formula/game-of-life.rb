7f15589cc39b07188bd8bcb89ed9913b7c1217cf
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-23 18:46:08 +0200

    game-of-life 1.7.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -1,9 +1,9 @@
 class GameOfLife < Formula
   desc "An implementation of Conway's Game of life"
   homepage "https://github.com/PurpleBooth/game-of-life"
-  url "https://github.com/PurpleBooth/game-of-life/archive/v1.7.0.tar.gz"
-  version "1.7.0"
-  sha256 "e3a2f9345fb5716cb4d7d2e2581e70d7a25fbba8df19bd1a627de1b8bb2449c1"
+  url "https://github.com/PurpleBooth/game-of-life/archive/v1.7.1.tar.gz"
+  version "1.7.1"
+  sha256 "4472a590148f6af1e57afbbd78f9da2eee27b4a6bc36a39c38f6fc9439aeb979"
 
   depends_on "rust" => :build
 
