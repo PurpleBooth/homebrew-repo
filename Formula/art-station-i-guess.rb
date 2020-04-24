f3d38059099a5078bf2fb640046278ebc8616cba
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-24 07:36:18 +0200

    art-station-i-guess 0.9.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -1,9 +1,9 @@
 class ArtStationIGuess < Formula
   desc "Download art from Art Station for wallpaper"
   homepage "https://github.com/PurpleBooth/art-station-i-guess"
-  url "https://github.com/PurpleBooth/art-station-i-guess/archive/v0.9.6.tar.gz"
-  version "0.9.6"
-  sha256 "795714c1a5a422de6e4a1e2b50026f4d40d1757e4070269ddc5a2a22e9c0d6ce"
+  url "https://github.com/PurpleBooth/art-station-i-guess/archive/v0.9.7.tar.gz"
+  version "0.9.7"
+  sha256 "10921f8e58e8cd2846294307166e4d48ec32b053ad8e87ec523c49d868388461"
 
   depends_on "rust" => :build
 
