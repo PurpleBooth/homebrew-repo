92c9af87303770dcfc3d86649cb5f304a21628f8
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-25 17:15:52 +0200

    pb-git-hooks 2.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/pb-git-hooks.rb
+++ Formula/pb-git-hooks.rb
@@ -1,9 +1,9 @@
 class PbGitHooks < Formula
   desc "My personal git commit hooks"
   homepage "https://github.com/PurpleBooth/pb-git-hooks"
-  url "https://github.com/PurpleBooth/pb-git-hooks/archive/v2.1.0.tar.gz"
-  version "2.1.0"
-  sha256 "9f7979ffb1a06b9983e4f8621ffc5551f7ed075e9361e8fb471e81ae323d3af5"
+  url "https://github.com/PurpleBooth/pb-git-hooks/archive/v2.2.0.tar.gz"
+  version "2.2.0"
+  sha256 "7e4545197472e167ad038e206df6ad26bc0785819851c3a911352f656f0650d2"
 
   depends_on "rust" => :build
 
