1e2cda3554f31978371b0054176cb6fa14f49a73
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-29 08:16:33 +0200

    Update fix-gnupg-permissions.rb

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -2,14 +2,12 @@ class FixGnupgPermissions < Formula
   desc "Fixes permissions problems on the gnupg directory"
   homepage "https://github.com/PurpleBooth/fix-gnupg-permissions"
   url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/v0.10.0.tar.gz"
-  version "0.10.0"
   sha256 "7829f5bc2e2ff2ececae3a370d6ca68ab67c880ce4b95dfa1a4bb9701cbd8e9b"
 
   depends_on "rust" => :build
 
   def install
-        ENV['VERSION'] = version
-        system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
