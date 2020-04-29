16d156c60a26836a884a8cb1af2b3f4781f89549
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-29 08:16:10 +0200

    Update fix-compaudit.rb

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -2,15 +2,13 @@ class FixCompaudit < Formula
   desc "Fixes problems reported by compuaudit"
   homepage "https://github.com/PurpleBooth/fix-compaudit"
   url "https://github.com/PurpleBooth/fix-compaudit/archive/v0.9.0.tar.gz"
-  version "0.9.0"
   sha256 "886b7d7d141717eb1bb7bc327d177a082d3ffc544d7c718c953cb2f844925842"
 
   depends_on "rust" => :build
   depends_on "zsh"
 
   def install
-        ENV['VERSION'] = version
-        system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
