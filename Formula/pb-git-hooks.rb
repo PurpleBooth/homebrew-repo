aba5e9a7f43e4dcb9c0ed619fcad1d06a750af6e
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-29 08:19:17 +0200

    Update pb-git-hooks.rb

--- Formula/pb-git-hooks.rb
+++ Formula/pb-git-hooks.rb
@@ -2,16 +2,14 @@ class PbGitHooks < Formula
   desc "My personal git commit hooks"
   homepage "https://github.com/PurpleBooth/pb-git-hooks"
   url "https://github.com/PurpleBooth/pb-git-hooks/archive/v2.11.0.tar.gz"
-  version "2.11.0"
   sha256 "29a0eaddb103aadb5f44505e041fe74b24a4323b2858a86c02d9406ecc20fa3f"
 
   depends_on "rust" => :build
 
   def install
-        ENV['VERSION'] = version
-        system "cargo", "install", "--locked", "--root", prefix, "--path", "./pb-commit-msg/"
-        system "cargo", "install", "--locked", "--root", prefix, "--path", "./pb-pre-commit/"
-        system "cargo", "install", "--locked", "--root", prefix, "--path", "./pb-prepare-commit-msg/"
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "./pb-commit-msg/"
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "./pb-pre-commit/"
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "./pb-prepare-commit-msg/"
   end
 
   test do
