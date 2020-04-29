39e028dd34958a6c520ef38716d7c17783cd2072
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-29 08:16:58 +0200

    Update game-of-life.rb

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -2,14 +2,12 @@ class GameOfLife < Formula
   desc "An implementation of Conway's Game of life"
   homepage "https://github.com/PurpleBooth/game-of-life"
   url "https://github.com/PurpleBooth/game-of-life/archive/v1.10.0.tar.gz"
-  version "1.10.0"
   sha256 "5cc4bb9e4f978bd0266e4206522b2e55f9a8f26dd9572cc37abc9f6171e673f1"
 
   depends_on "rust" => :build
 
   def install
-        ENV['VERSION'] = version
-        system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
