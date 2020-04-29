bbdf03b6716086f829b52dcf81e123184bcd47d3
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-29 08:19:48 +0200

    Update setup-a-keyring-for-sops.rb

--- Formula/setup-a-keyring-for-sops.rb
+++ Formula/setup-a-keyring-for-sops.rb
@@ -14,15 +14,13 @@ class SetupAKeyringForSops < Formula
   desc "Use gcloud to setup everything needed for SOPS"
   homepage "https://github.com/PurpleBooth/setup-a-keyring-for-sops"
   url "https://github.com/PurpleBooth/setup-a-keyring-for-sops/archive/v0.8.0.tar.gz"
-  version "0.8.0"
   sha256 "f2e37b99b5630f2839388273703a350d88884c07327732944d5e9af8b0cfcbf7"
 
   depends_on "rust" => :build
   depends_on GCloudRequirement
 
   def install
-        ENV['VERSION'] = version
-        system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
