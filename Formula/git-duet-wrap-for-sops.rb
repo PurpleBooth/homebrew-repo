ec781ff910d86d3a438031b3aa7a89e948865b63
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-29 08:39:11 +0200

    Update git-duet-wrap-for-sops.rb

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -1,5 +1,5 @@
 class GitDuetWrapForSops < Formula
-  desc "A wrapper for the git duet commands that lets you leave your authors file encrypted"
+  desc "Keeps your git authors file encrypted"
   homepage "https://github.com/PurpleBooth/git-duet-wrap-for-sops"
   url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/v0.3.0.tar.gz"
   sha256 "62be04a0b68499689872e3d713011246621758499302343076dbb0e09d69f349"
