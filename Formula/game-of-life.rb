32748c5326696493092af9c778266a0acd4e64a1
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-29 08:26:44 +0200

    Update game-of-life.rb

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -1,5 +1,5 @@
 class GameOfLife < Formula
-  desc "An implementation of Conway's Game of life"
+  desc "PurpleBooth's implementation of Conway's Game of life"
   homepage "https://github.com/PurpleBooth/game-of-life"
   url "https://github.com/PurpleBooth/game-of-life/archive/v1.10.0.tar.gz"
   sha256 "5cc4bb9e4f978bd0266e4206522b2e55f9a8f26dd9572cc37abc9f6171e673f1"
