efd3315d7211c4d287fecbd821417e6db8472aec
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-29 07:14:43 +0200

    Update git-duet-wrap-for-sops.rb

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -3,7 +3,7 @@ class GitDuetWrapForSops < Formula
   homepage "https://github.com/PurpleBooth/git-duet-wrap-for-sops"
   url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/v0.3.0.tar.gz"
   version "0.3.0"
-  sha256 ""
+  sha256 "62be04a0b68499689872e3d713011246621758499302343076dbb0e09d69f349"
 
   depends_on "rust" => :build
 
