fb6bfcb6ba1cef925ea68fe49411045faebe3679
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-05-26 18:04:41 +0200

    fix-gnupg-permissions 0.10.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -1,8 +1,8 @@
 class FixGnupgPermissions < Formula
   desc "Fixes permissions problems on the gnupg directory"
   homepage "https://github.com/PurpleBooth/fix-gnupg-permissions"
-  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/v0.10.6.tar.gz"
-  sha256 "17a3f44d3b2953cfc9155597fe4e9894beebf8988534107dfc1b31902dba6458"
+  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/v0.10.7.tar.gz"
+  sha256 "b6c642958f4a853cfb6dce6665b5a374cac764aa47a7d5a51acf0eceb48f62bd"
 
   depends_on "rust" => :build
 
