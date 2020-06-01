0a21fe9bc4541c83fc697075d7485cc2e3d04101
Billie Thompson <billie@billiecodes.com>
2020-06-01 23:18:30 +0200

    Add the repository

---
+++ Formula/goodreads-quotes.rb
@@ -0,0 +1,17 @@
+class GameOfLife < Formula
+  desc "Get a random quote from random categories in goodreads"
+  homepage "https://github.com/PurpleBooth/goodreads-quotes"
+  url "https://github.com/PurpleBooth/goodreads-quotes/archive/x.tar.gz"
+  sha256 "unknown"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/goodreads-quotes", "-h"
+    system "#{bin}/goodreads-quotes", "-V"
+  end
+end
