bf3c9a345e9a9704023a0c3184c1f86a3617b862
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-06-02 09:46:22 +0200

    goodreads-quotes 0.4.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/goodreads-quotes.rb
+++ Formula/goodreads-quotes.rb
@@ -1,8 +1,8 @@
 class GoodreadsQuotes < Formula
   desc "Get a random quote from random categories in goodreads"
   homepage "https://github.com/PurpleBooth/goodreads-quotes"
-  url "https://github.com/PurpleBooth/goodreads-quotes/archive/v0.3.0.tar.gz"
-  sha256 "43135e27126ec51fe14919d0300d304d01cb73f95c24b568d069a7625a343570"
+  url "https://github.com/PurpleBooth/goodreads-quotes/archive/v0.4.0.tar.gz"
+  sha256 "9178d706f0756a2d943d42c5f2b1f1508d6300529d7bf0e76ead61c5f0bf7cfa"
 
   depends_on "rust" => :build
 
