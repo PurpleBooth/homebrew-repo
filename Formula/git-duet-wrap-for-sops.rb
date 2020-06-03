6b4a4c64f79218ebb02833ee2cc90ab271747b15
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-06-03 17:49:21 +0200

    git-duet-wrap-for-sops 0.10.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -1,8 +1,8 @@
 class GitDuetWrapForSops < Formula
   desc "Keeps your git authors file encrypted"
   homepage "https://github.com/PurpleBooth/git-duet-wrap-for-sops"
-  url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/v0.3.2.tar.gz"
-  sha256 "15c7e904ff34b8108c86c04a4029758e390864bf64c9a1e97cda005dd89e52a6"
+  url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/0.10.0.tar.gz"
+  sha256 "8251a9eff318221fadad567a2f46069d6d29819b568ac5087cf8af2f0f4d7a14"
 
   depends_on "rust" => :build
 
