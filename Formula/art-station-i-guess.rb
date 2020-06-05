b918ca3253c3ef0970b027fa4fd2122fc66ffd3d
runner <runner@Mac-1820.local>
2020-06-05 11:52:38 +0000

    art-station-i-guess: add 0.19.0 bottle.

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -4,6 +4,12 @@ class ArtStationIGuess < Formula
   url "https://github.com/PurpleBooth/art-station-i-guess/archive/v0.19.0.tar.gz"
   sha256 "0f6cd02f9c50a7242e1756651da420d86ab39ffebd6d975fc6a6943895d091a0"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "0df767b03efce3871ff775da68ab249da6f27bd530073dc96d7c7bfc481a9e37" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
