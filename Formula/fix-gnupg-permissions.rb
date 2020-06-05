8a05e3f30d5c30c83710fc30a804db77572ab747
runner <runner@Mac-1820.local>
2020-06-05 11:52:40 +0000

    fix-gnupg-permissions: add 0.16.0 bottle.

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -4,6 +4,12 @@ class FixGnupgPermissions < Formula
   url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/v0.16.0.tar.gz"
   sha256 "3f2a6622556a051644de0425a94591d9fdb21424546a776dbad699fd7340cc26"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "7026a8aede660fd87e8758a229ecf3a86b39d6b0d8ab1f6cc159379e6af741af" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
