c93ee52ae18371927374fb8403a8d8d81bab21ad
runner <runner@Mac-1820.local>
2020-06-05 11:52:46 +0000

    setup-a-keyring-for-sops: add 0.14.0 bottle.

--- Formula/setup-a-keyring-for-sops.rb
+++ Formula/setup-a-keyring-for-sops.rb
@@ -17,6 +17,12 @@ class SetupAKeyringForSops < Formula
   url "https://github.com/PurpleBooth/setup-a-keyring-for-sops/archive/v0.14.0.tar.gz"
   sha256 "d36826e7fab5b5a7394f92e8d19717a61de9fc8867eff2064ffab165c47efe90"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "6f090ce14855a4b3d2c3ee2f5476682b4f0dfedd682277a0cc7ddc49b9a8883e" => :catalina
+  end
+
   depends_on "rust" => :build
   depends_on GCloudRequirement
 
