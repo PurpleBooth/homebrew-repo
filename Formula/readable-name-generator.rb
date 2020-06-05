a3ca929d00c9fcb0ae79e3bb8acf2fbb37b412d3
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-06-05 10:09:07 +0200

    readable-name-generator 2.11.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -1,8 +1,8 @@
 class ReadableNameGenerator < Formula
   desc "Generate a readable name using Dockers formula"
   homepage "https://github.com/PurpleBooth/readable-name-generator"
-  url "https://github.com/PurpleBooth/readable-name-generator/archive/v2.10.0.tar.gz"
-  sha256 "7363194bc295e2164e4608a9e23c557da677334cabc16780fed0b42ad29bbd90"
+  url "https://github.com/PurpleBooth/readable-name-generator/archive/v2.11.0.tar.gz"
+  sha256 "ae842abd30c9166a2e9f46388f6cc373117a648ab0f9548fa1b22859daa5e518"
 
   depends_on "go" => :build
 
