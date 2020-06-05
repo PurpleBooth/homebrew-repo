87ab86b34fa9032d2ae78386703558438a1fbb6d
Billie Thompson <billie@billiecodes.com>
2020-06-05 16:53:02 +0200

    Try another apporach

--- bin/git-pull-git-push-tap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -xeuo pipefail
-
-{
-	cd "$(brew --repo)/Library/Taps/purplebooth/homebrew-repo"
-	git pull --rebase
-	git push
-}
--- bin/set-user.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -xeuo pipefail
-
-git config --global user.name "$GIT_USER_NAME"
-git config --global user.email "$GIT_USER_EMAIL"
