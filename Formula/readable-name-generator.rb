b84e79879ff80903a8445b2293cfdc7d0e0a6d44
runner <runner@Mac-1820.local>
2020-06-05 11:52:45 +0000

    readable-name-generator: add 2.11.0 bottle.

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -4,6 +4,12 @@ class ReadableNameGenerator < Formula
   url "https://github.com/PurpleBooth/readable-name-generator/archive/v2.11.0.tar.gz"
   sha256 "ae842abd30c9166a2e9f46388f6cc373117a648ab0f9548fa1b22859daa5e518"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "46e6678e38c1c169eb14260e129402b0f31be3a91c99049f7c36115d4947957e" => :catalina
+  end
+
   depends_on "go" => :build
 
   def install
