7ccdfef091714e798b22d05a96860769bf977c90
Billie Thompson <billie@billiecodes.com>
2020-06-05 14:43:25 +0200

    Bump the version

---
+++ bin/bump-all-revision.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+PACKAGES=()
+
+for I in Formula/*.rb; do
+	PACKAGES+=("purplebooth/repo/$(echo "$I" | sed -e 's/^Formula\/*//' | sed -e 's/.rb$//')")
+done
+
+brew bump-revision "${PACKAGES[@]}"
