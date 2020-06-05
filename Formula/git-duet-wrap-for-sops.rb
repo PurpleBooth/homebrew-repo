79cf3c9af1f34f1f774b1ae195272da3a3ac2943
runner <runner@Mac-1820.local>
2020-06-05 11:52:42 +0000

    git-duet-wrap-for-sops: add 0.16.0 bottle.

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -4,6 +4,12 @@ class GitDuetWrapForSops < Formula
   url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/v0.16.0.tar.gz"
   sha256 "2a03c0a5d19d075748b0925f92d4145f504b0e840dcee90c2d8c18650feba87d"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "86d040c7ad1bc6fc03e685583ef34147834763a982af8fcdd1342b3b11ef04e0" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
