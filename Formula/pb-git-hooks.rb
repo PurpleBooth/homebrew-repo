ce11a4f7ab7749cc7b8ce8a3d6a299e859f1edfe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-07 21:28:27 +0000

    pb-git-hooks: update 2.110.1 bottle.

--- Formula/pb-git-hooks.rb
+++ Formula/pb-git-hooks.rb
@@ -3,6 +3,12 @@ class PbGitHooks < Formula
   homepage "https://github.com/PurpleBooth/pb-git-hooks"
   url "https://github.com/PurpleBooth/pb-git-hooks/archive/refs/tags/v2.110.1.tar.gz"
   sha256 "aac35db74da2e8f6311a5f638f1cb1bb62d8d64baa63e7c2c5e39e86be053376"
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any
+    sha256 "b975e2a16f41e97a63abf2c06bb5ae4f0c9edaa525400064bef49a3ec9511add" => :catalina
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
