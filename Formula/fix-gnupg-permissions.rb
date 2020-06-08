46e5fa2d83896317c0adc09c32c54c2d357fdc13
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-08 06:56:53 +0000

    fix-gnupg-permissions: update 0.23.0 bottle.

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -1,13 +1,13 @@
 class FixGnupgPermissions < Formula
   desc "Fixes permissions problems on the gnupg directory"
   homepage "https://github.com/PurpleBooth/fix-gnupg-permissions"
-  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.22.0.tar.gz"
-  sha256 "be93f384de391ebff8d6d5cf7bd3dc0c41cfd60207fa5bc703a37a17f246f59d"
+  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.23.0.tar.gz"
+  sha256 "e61cbb4aac4c7db6988c2616217e42fe995e3ca665da6aca24f304e0c097de36"
 
   bottle do
     root_url "https://dl.bintray.com/purplebooth/bottles-repo"
     cellar :any_skip_relocation
-    sha256 "cefa8f6683e483276bde4075e55ffd7cb24d7be7d770c89080a62f4a494a2289" => :catalina
+    sha256 "3a870f32e701f60d6d75537d56f219bc9a7f13799140cbb77683e49ffb1407ef" => :catalina
   end
 
   depends_on "rust" => :build
