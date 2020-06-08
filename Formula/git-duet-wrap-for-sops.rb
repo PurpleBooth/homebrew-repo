5ae38aa3c2d9ad65e5d9239446f08214f8b3a9f7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-08 06:57:06 +0000

    git-duet-wrap-for-sops: update 0.22.0 bottle.

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -1,13 +1,13 @@
 class GitDuetWrapForSops < Formula
   desc "Keeps your git authors file encrypted"
   homepage "https://github.com/PurpleBooth/git-duet-wrap-for-sops"
-  url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.21.0.tar.gz"
-  sha256 "d4ec9c61cf4e51e44fc951dfc183deeaecc14b1469c16bbaa32d46584d10bf8f"
+  url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.22.0.tar.gz"
+  sha256 "90b340dcf49f5e437126a15588ff23569d5ce102d9df41fbd2ee443f24571b48"
 
   bottle do
     root_url "https://dl.bintray.com/purplebooth/bottles-repo"
     cellar :any_skip_relocation
-    sha256 "764db78f4a66204b45a6f91104401ced23cc4c74385dd9148f16b60d6c337442" => :catalina
+    sha256 "e922ef23a2919a608d8783a602eaed5fcf7baf417ea7a64025af3f860cb6f2d2" => :catalina
   end
 
   depends_on "rust" => :build
