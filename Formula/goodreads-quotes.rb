a014da4de837a8ddb0066ecb635d5c820ea91b37
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-08 07:56:49 +0000

    goodreads-quotes: update 0.17.1 bottle.

--- Formula/goodreads-quotes.rb
+++ Formula/goodreads-quotes.rb
@@ -1,13 +1,13 @@
 class GoodreadsQuotes < Formula
   desc "Get a random quote from random categories in goodreads"
   homepage "https://github.com/PurpleBooth/goodreads-quotes"
-  url "https://github.com/PurpleBooth/goodreads-quotes/archive/refs/tags/v0.17.0.tar.gz"
-  sha256 "017a486321b70eafd66e99e610724fe24a481b391b7695d99793ab7d624b5986"
+  url "https://github.com/PurpleBooth/goodreads-quotes/archive/refs/tags/v0.17.1.tar.gz"
+  sha256 "565fd7ec0d408191113566a7f996cd27b5087555bd08e5180111313342489e84"
 
   bottle do
     root_url "https://dl.bintray.com/purplebooth/bottles-repo"
     cellar :any_skip_relocation
-    sha256 "c927336d9017ba8e54ac8ca4ebbbf4298fbf5a6e310749685e4ff9a08c04960d" => :catalina
+    sha256 "3aee367fa53d457734fbb6a45df41cf80939ff2aa964f4e6553030e1339e926f" => :catalina
   end
 
   depends_on "rust" => :build
