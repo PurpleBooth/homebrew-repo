635b8ff0552bee4ab900f34cb53d9febd4734780
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-08 07:29:11 +0000

    art-station-i-guess: update 0.25.1 bottle.

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -1,13 +1,13 @@
 class ArtStationIGuess < Formula
   desc "Download art from Art Station for wallpaper"
   homepage "https://github.com/PurpleBooth/art-station-i-guess"
-  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.25.0.tar.gz"
-  sha256 "7d14df62cca70a1a9665a51588132f134c2e535e04ad4bb9364c4ffbc902b4ec"
+  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.25.1.tar.gz"
+  sha256 "968aaf250f9d02643330e1e2e3cad2caa11fdde2aede0c86365eabe95858c327"
 
   bottle do
     root_url "https://dl.bintray.com/purplebooth/bottles-repo"
     cellar :any_skip_relocation
-    sha256 "c794ad522eb07d1a58ac866750e2ea48e338c64227772ba5e096beda83333c8a" => :catalina
+    sha256 "5d06302e997f125d80991bad67b81cebbb6ae3bb7a0880cf373f43458bdad90a" => :catalina
   end
 
   depends_on "rust" => :build
