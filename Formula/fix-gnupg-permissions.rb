d99959ef674ced6ae6d73c7c0a26ab06df18b6b1
runner <runner@Mac-1682.local>
2020-06-09 23:39:50 +0000

    Update fix-gnupg-permissions to refs/tags/v0.30.0

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -1,8 +1,8 @@
 class FixGnupgPermissions < Formula
   desc "Fixes permissions problems on the gnupg directory"
   homepage "https://github.com/PurpleBooth/fix-gnupg-permissions"
-  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.29.0.tar.gz"
-  sha256 "229b3c2f207e3c8a44f12886fdb302b286f8cc16f3ad7b4cbf0b8f8ee53953cf"
+  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.30.0.tar.gz"
+  sha256 "5e7451529f75d726939dba093d4063bcb13992d185b3cfbbb31350b0127d855b"
 
   depends_on "rust" => :build
 
