975f66301c32ae494f7cd7dee02292d5979d738b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-10 18:31:12 +0000

    readable-name-generator: update 2.40.0 bottle.

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -4,6 +4,12 @@ class ReadableNameGenerator < Formula
   url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v2.40.0.tar.gz"
   sha256 "573fae2671bcb7f8d44deac032dcfd1e69f0a85093afb8feb92ea9ce837eba6a"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "a364989a63306c5192f5bac74cc3afaea3ca128b754ef7a34d09941e18ad8cd7" => :catalina
+  end
+
   depends_on "go" => :build
 
   def install
