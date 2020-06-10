e2d5862489a6a70b4587ec669bfcc8304c73549f
runner <runner@Mac-1682.local>
2020-06-10 05:30:14 +0000

    Update art-station-i-guess to refs/tags/v0.32.1

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -1,8 +1,8 @@
 class ArtStationIGuess < Formula
   desc "Download art from Art Station for wallpaper"
   homepage "https://github.com/PurpleBooth/art-station-i-guess"
-  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.32.0.tar.gz"
-  sha256 "e102c9c58eb8fe08d999f6139504ff11fd5bf1f1ea0f064b962fea993b1a21a9"
+  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.32.1.tar.gz"
+  sha256 "abf6b57f4179683a001a0cd6eb32ff1d81f6247469d960963153fff206bfb33a"
 
   depends_on "rust" => :build
 
