ac2f3ff57ea2c0f884516fba60ce454fb7d32d3c
runner <runner@Mac-1467.local>
2020-06-10 17:10:25 +0000

    Update readable-name-generator to refs/tags/v2.39.0

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -1,8 +1,8 @@
 class ReadableNameGenerator < Formula
   desc "Generate a readable name using Dockers formula"
   homepage "https://github.com/PurpleBooth/readable-name-generator"
-  url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v2.38.0.tar.gz"
-  sha256 "8e44b23d231d6b837bf835816fd55853f0084091a1db879199fa6ee4a589fa5a"
+  url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v2.39.0.tar.gz"
+  sha256 "5b91fd85556e18bc65f22c0870ceab74e83dd45ac4d2287ebbab4e836f8040b1"
 
   depends_on "go" => :build
 
