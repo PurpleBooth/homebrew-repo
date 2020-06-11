4f4d803b84a45c73b1790e11c8be83c6aaf3c8fb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-11 08:04:04 +0000

    art-station-i-guess: update 0.37.0 bottle.

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -4,6 +4,12 @@ class ArtStationIGuess < Formula
   url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.37.0.tar.gz"
   sha256 "5ed7d255ae6f094c650c1e274e96779d387c7855e66d16dae1e2bc90887a1f7a"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "e38eb7180e78b48d8f1eb9a3a3511a917a8ad739ada3edc65c3bccbc1b7919d8" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
