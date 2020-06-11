d702892e199abba6c1210908f50cacec42908563
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-06-11 10:51:37 +0200

    art-station-i-guess: update 0.38.0 bottle. (#44)
    Co-authored-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -4,6 +4,12 @@ class ArtStationIGuess < Formula
   url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.38.0.tar.gz"
   sha256 "ad950a56893882e326b15208b38c64d8e9827fb14136016c9a56f04e56771795"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "89f8aeb72b60c8b6e344ba4ae840dfe996f3a9cb3b56de96dee440d9e6cb2f0f" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
