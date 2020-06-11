dab7890abf243732e5b92d625d90aa29a4f3e3c7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-11 08:33:20 +0000

    setup-a-keyring-for-sops: update 0.41.0 bottle.

--- Formula/setup-a-keyring-for-sops.rb
+++ Formula/setup-a-keyring-for-sops.rb
@@ -4,6 +4,12 @@ class SetupAKeyringForSops < Formula
   url "https://github.com/PurpleBooth/setup-a-keyring-for-sops/archive/refs/tags/v0.41.0.tar.gz"
   sha256 "12576f2ca5b830f32a414e5b677b63ba11f84dd925f48e5d9788afd04a31d752"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "3287da029de864a0589477bb194af3f23bfabfd21c3fc6885867ad483387bba8" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
