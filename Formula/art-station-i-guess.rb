3dc8ccfe079b5b6e7372f81ab22c31123c0ff3a5
runner <runner@Mac-1467.local>
2020-06-12 07:55:03 +0000

    Update art-station-i-guess to v0.41.0

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -1,14 +1,8 @@
 class ArtStationIGuess < Formula
   desc "Download art from Art Station for wallpaper"
   homepage "https://github.com/PurpleBooth/art-station-i-guess"
-  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.40.0.tar.gz"
-  sha256 "698e5c65612870e6c17838bf57e24da2ca53701ca1916b113398c8290f6ce436"
-
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "4e4c01926bb9771dc0d6dc199bc8d55d0c07c0ed7434eeec5aa3fb27ae2e443e" => :catalina
-  end
+  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.41.0.tar.gz"
+  sha256 "71d4f1e8a86d786ee9b721104ade42997a7e85c61da0715468d7b1cc353bc220"
 
   depends_on "rust" => :build
 
