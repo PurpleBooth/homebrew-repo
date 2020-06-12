55d96ec493d929aa518dfbf959f4b58858303d3b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-12 07:38:35 +0000

    fix-compaudit: update 0.29.0 bottle.

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -4,6 +4,12 @@ class FixCompaudit < Formula
   url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.29.0.tar.gz"
   sha256 "4c0353cfc9bf8b146e9e857e31636bb5cc1a36d44eb8ae927136662edac1521a"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "446cd10e691ec9edf9542ffc0fc1183f5028e44e042b12809b6660acc8de88c3" => :catalina
+  end
+
   depends_on "rust" => :build
   depends_on "zsh"
 
