74be702e1c37aeb609beac1cc9d22daf7c1a73c1
runner <runner@Mac-1467.local>
2020-06-12 23:06:37 +0000

    Update pb-git-hooks to v2.144.0

--- Formula/pb-git-hooks.rb
+++ Formula/pb-git-hooks.rb
@@ -1,14 +1,8 @@
 class PbGitHooks < Formula
   desc "My personal git commit hooks"
   homepage "https://github.com/PurpleBooth/pb-git-hooks"
-  url "https://github.com/PurpleBooth/pb-git-hooks/archive/refs/tags/v2.143.0.tar.gz"
-  sha256 "12a32a0b3c5e20f2ea185abb83931de0cef67dda137fb913daf609e7d853b0a5"
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any
-    sha256 "02b3e8128c318d1d92739311a85b83257102afa2ca34b8f87a2875629f8cda74" => :catalina
-  end
-
+  url "https://github.com/PurpleBooth/pb-git-hooks/archive/refs/tags/v2.144.0.tar.gz"
+  sha256 "bf33855fc70a867cd9764192f381d0d87697e7c2af96cf4fc29ea01191b60b1f"
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
