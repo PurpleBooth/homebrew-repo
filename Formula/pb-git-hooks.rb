55d9d1199016c4f5204acf5c09eb4486394fd61c
runner <runner@Mac-1467.local>
2020-06-13 14:41:02 +0000

    Update pb-git-hooks to v2.147.0

--- Formula/pb-git-hooks.rb
+++ Formula/pb-git-hooks.rb
@@ -1,8 +1,8 @@
 class PbGitHooks < Formula
   desc "My personal git commit hooks"
   homepage "https://github.com/PurpleBooth/pb-git-hooks"
-  url "https://github.com/PurpleBooth/pb-git-hooks/archive/refs/tags/v2.146.0.tar.gz"
-  sha256 "2e1f1fd1df392f8f6e9286f28e17006a797900f2400952f7c816fa38e26a31df"
+  url "https://github.com/PurpleBooth/pb-git-hooks/archive/refs/tags/v2.147.0.tar.gz"
+  sha256 "c8ec8a3974ae78b39b714aa23c36ca5ed9e43d745d6be3c9cdd6b9b912163243"
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
