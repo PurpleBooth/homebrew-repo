241e6e9b923f7a478459ff92a1fde7134035b082
runner <runner@Mac-1467.local>
2020-06-14 08:46:37 +0000

    Update git-mit to v3.0.0

---
+++ Formula/git-mit.rb
@@ -0,0 +1,29 @@
+class GitMit < Formula
+  desc "My personal git commit hooks"
+  homepage "https://github.com/PurpleBooth/git-mit"
+  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "f3c351c556b73b270a7d44ad4b7eb1950823c08ee9830d46b678252404f529c2"
+  depends_on "rust" => :build
+  depends_on "openssl@1.1"
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "./mit-commit-msg/"
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "./mit-pre-commit/"
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "./mit-prepare-commit-msg/"
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "./git-mit/"
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "./git-mit-config/"
+  end
+
+  test do
+    system "#{bin}/mit-commit-msg", "-h"
+    system "#{bin}/mit-commit-msg", "-V"
+    system "#{bin}/mit-pre-commit", "-h"
+    system "#{bin}/mit-pre-commit", "-V"
+    system "#{bin}/mit-prepare-commit-msg", "-h"
+    system "#{bin}/mit-prepare-commit-msg", "-V"
+    system "#{bin}/git-mit", "-h"
+    system "#{bin}/git-mit", "-V"
+    system "#{bin}/git-mit-config", "-h"
+    system "#{bin}/git-mit-config", "-V"
+  end
+end
