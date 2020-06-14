3ca4c81cc953bddf75702be70f33c721ac5c48d1
runner <runner@Mac-1467.local>
2020-06-14 22:02:40 +0000

    Update git-mit to v3.3.0

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,14 +1,8 @@
 class GitMit < Formula
   desc "My personal git commit hooks"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.2.0.tar.gz"
-  sha256 "7373b91f356946c1620abe40abb829a9525a4c933fb0ca2a9a2d7035c575159f"
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any
-    sha256 "3a203efe1b60c5c8020a4aa7482495f4ca7e93932d777fa03b3cb65f06043734" => :catalina
-  end
-
+  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.3.0.tar.gz"
+  sha256 "4eb91c9963322491f9d9bc1bc259c2f8faacd0d3aee6dd0e203c649b826cfcea"
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
