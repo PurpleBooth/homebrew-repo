db677294435fe354eff382ae666cd71434f933f0
runner <runner@Mac-1467.local>
2020-06-15 06:21:17 +0000

    Update goodreads-quotes to v0.44.0

--- Formula/goodreads-quotes.rb
+++ Formula/goodreads-quotes.rb
@@ -1,8 +1,8 @@
 class GoodreadsQuotes < Formula
   desc "Get a random quote from random categories in goodreads"
   homepage "https://github.com/PurpleBooth/goodreads-quotes"
-  url "https://github.com/PurpleBooth/goodreads-quotes/archive/refs/tags/v0.45.0.tar.gz"
-  sha256 "12349f7e9f322e84d2ac4c18f28bec831b41697094d4886c28c878878446b5d2"
+  url "https://github.com/PurpleBooth/goodreads-quotes/archive/refs/tags/v0.44.0.tar.gz"
+  sha256 "d80877a2bdbcd4052eb13c9d748b38dcd09b0a180302f06130dc43339c1c62b7"
 
   depends_on "rust" => :build
 
