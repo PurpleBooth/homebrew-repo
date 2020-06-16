da6e997264da803b7620c54dda4c7404b7b261c4
runner <runner@Mac-1467.local>
2020-06-16 05:35:05 +0000

    Update goodreads-quotes to v0.48.0

--- Formula/goodreads-quotes.rb
+++ Formula/goodreads-quotes.rb
@@ -1,14 +1,8 @@
 class GoodreadsQuotes < Formula
   desc "Get a random quote from random categories in goodreads"
   homepage "https://github.com/PurpleBooth/goodreads-quotes"
-  url "https://github.com/PurpleBooth/goodreads-quotes/archive/refs/tags/v0.47.0.tar.gz"
-  sha256 "458944446ef40f9e1aebf0e67e73a909b569a0d9d25a470f678bec55d0274c38"
-
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "d36ea17604383adf525ce9af7521408a451fa9d46e69df75437f0e7d72ab4b3b" => :catalina
-  end
+  url "https://github.com/PurpleBooth/goodreads-quotes/archive/refs/tags/v0.48.0.tar.gz"
+  sha256 "fc052d9467a7419ad8e8c882db94561e99c069a8c9faf84c178e50fe453ac233"
 
   depends_on "rust" => :build
 
