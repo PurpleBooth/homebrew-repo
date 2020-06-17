6a4f2018efa65f58928e115a4b84caa42a0d67c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-17 06:13:19 +0000

    fix-gnupg-permissions: update 0.37.0 bottle.

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -4,6 +4,12 @@ class FixGnupgPermissions < Formula
   url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.37.0.tar.gz"
   sha256 "8d9aa5b35a90528a03b4691f3a49a625978a6ded2b21cd0782a4baaf519f77f9"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "43932002b053c8129da5ffc1b167f41421229ce807ec99b970f5e946da082292" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
