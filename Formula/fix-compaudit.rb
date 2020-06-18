70be5f7a062c7afa657ea365f4c8b30d05ed62b2
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-06-18 08:08:25 +0200

    Update fix-compaudit.rb

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -4,12 +4,6 @@ class FixCompaudit < Formula
   url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.36.0.tar.gz"
   sha256 "455fa63b947aa905e1cf2d8445432fbe97cbfd2b4d9f3015ad1aaa4ea1e5a837"
 
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "3456ae95a1c0cefa2fa0dbea1b909223a4d75b1a9adbeb63c879223d19bf9d96" => :catalina
-  end
-
   depends_on "rust" => :build
   depends_on "zsh"
 
