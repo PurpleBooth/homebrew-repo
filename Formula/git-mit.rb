b40d49fce1ff2470133a8a5f917306cf6b8c627b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-18 05:59:39 +0000

    git-mit: update 3.7.0 bottle.

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -3,6 +3,12 @@ class GitMit < Formula
   homepage "https://github.com/PurpleBooth/git-mit"
   url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.7.0.tar.gz"
   sha256 "c1099d890d30ae46a4cd49e88873c47ea3dca3c0571bf5f2c0a09cd9816c0f30"
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any
+    sha256 "6eab3982a8bab7ec52674c44c37540791e5f1fc5300569d7035dc30774bf3908" => :catalina
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
