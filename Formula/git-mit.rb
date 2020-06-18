52d025d09c2501150c34e9f27127e5139f69a979
runner <runner@Mac-1467.local>
2020-06-18 20:42:13 +0000

    Update git-mit to v3.12.0

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,14 +1,8 @@
 class GitMit < Formula
   desc "My personal git commit hooks"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.11.0.tar.gz"
-  sha256 "07493eeb694befc6a90b3e71d82eb96e81ddfbb236f7b40f1a8c9b28d36b6510"
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any
-    sha256 "028c657ffbc41b6da4872b5d92ee8aba1116572c95905165ee87313fa286a57f" => :catalina
-  end
-
+  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.12.0.tar.gz"
+  sha256 "9306c88847c95837c9cd3bec0108e433c5be7c9c8145e7ccdec4d6bfbe1a8b43"
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
@@ -19,23 +13,17 @@ class GitMit < Formula
     system "cargo", "install", "--locked", "--root", prefix, "--path", "./git-mit/"
     system "cargo", "install", "--locked", "--root", prefix, "--path", "./git-mit-config/"
 
-    output = Utils.popen_read("#{bin}/git-mit --completion bash")
-    (bash_completion/"git-mit").write output
+    Pathname.glob('**/bash_completion/*').each do |file|
+      bash_completion.install file
+    end
 
-    output = Utils.popen_read("#{bin}/git-mit --completion zsh")
-    (zsh_completion/"_git-mit").write output
+    Pathname.glob('**/fish_completion/*').each do |file|
+      fish_completion.install file
+    end
 
-    output = Utils.popen_read("#{bin}/git-mit --completion fish")
-    (fish_completion/"git-mit.fish").write output
-
-    output = Utils.popen_read("#{bin}/git-mit-config completion bash")
-    (bash_completion/"git-mit-config").write output
-
-    output = Utils.popen_read("#{bin}/git-mit-config completion zsh")
-    (zsh_completion/"_git-mit-config").write output
-
-    output = Utils.popen_read("#{bin}/git-mit-config completion fish")
-    (fish_completion/"git-mit-config.fish").write output
+    Pathname.glob('**/zsh_completion/*').each do |file|
+      zsh_completion.install file
+    end
   end
 
   test do
