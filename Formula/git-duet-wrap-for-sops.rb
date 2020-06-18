b779585d40feccd076bbab8231608086fcffeb76
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-06-18 07:55:00 +0200

    Update git-duet-wrap-for-sops.rb (#191)

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -4,18 +4,6 @@ class GitDuetWrapForSops < Formula
   url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.35.0.tar.gz"
   sha256 "9ddac66e8da744a467406afa95a92420a9cb8ed608d7a76ae2ded92f8204a89d"
 
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "ad92f28ed379858a7a7407358db6748e3f6fa01531e4ecca97a6b64bfafb132c" => :catalina
-  end
-
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "d8341ebc72cb190a5b0c6d8b0cf33dad2eb3b4655845ce9f988febe11fff310c" => :catalina
-  end
-
   depends_on "rust" => :build
 
   def install
