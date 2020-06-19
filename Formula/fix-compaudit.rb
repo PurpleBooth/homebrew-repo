5f469d43ed84a1c5e5e7822f27edf28ece82e359
runner <runner@Mac-1467.local>
2020-06-19 05:32:30 +0000

    Update fix-compaudit to v0.37.0

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -1,8 +1,8 @@
 class FixCompaudit < Formula
   desc "Fixes problems reported by compuaudit"
   homepage "https://github.com/PurpleBooth/fix-compaudit"
-  url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.36.0.tar.gz"
-  sha256 "455fa63b947aa905e1cf2d8445432fbe97cbfd2b4d9f3015ad1aaa4ea1e5a837"
+  url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.37.0.tar.gz"
+  sha256 "f5ec924e60d863f2c2de42a12b02b1e687394d303818e044b2c4d7739e226bf5"
 
   depends_on "rust" => :build
   depends_on "zsh"
