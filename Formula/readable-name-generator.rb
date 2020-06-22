f3c9292c7f4d66579c5843450e7127cdd66155a9
runner <runner@Mac-1467.local>
2020-06-22 05:44:52 +0000

    Update readable-name-generator to v2.53.0

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -1,8 +1,14 @@
 class ReadableNameGenerator < Formula
   desc "Generate a readable name using Dockers formula"
   homepage "https://github.com/PurpleBooth/readable-name-generator"
-  url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v2.52.0.tar.gz"
-  sha256 "48a7d5250066c47454491d78bf6555256976e4807f3e3eaedf18d9d8104cb445"
+  url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v2.53.0.tar.gz"
+  sha256 "eb39a4cec363f49e7ca4467e07bbfcd8e196d831f7c9cae700889c45ca89655e"
+
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "6a22fbe717c4d3a5ed8f58bcd644b49ebd9d6c12a810f3ad40122029f87bd02c" => :catalina
+  end
 
   depends_on "go" => :build
 
