4ca906bb5eabdc01c1de3e189cb682c489724ed9
runner <runner@Mac-1467.local>
2020-06-23 20:02:13 +0000

    Update git-mit to v3.44.0

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,8 +1,8 @@
 class GitMit < Formula
   desc "My personal git commit hooks"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.43.0.tar.gz"
-  sha256 "c2f8c323eb3e229f03c5171aa955ac673007521200c6aa9041d1cc510606ac6b"
+  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.44.0.tar.gz"
+  sha256 "13f5ef43bc578dacfcd231ffc587afd3632f835c3e0d88ecd6e662ee251e8fed"
   depends_on "pandoc" => :build
   depends_on "rust" => :build
   depends_on "openssl@1.1"
