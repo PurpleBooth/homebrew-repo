638dfc0c02af2246592e9191662f14f5a4f011cc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-30 06:01:11 +0000

    game-of-life: update 1.42.0 bottle.

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -4,6 +4,12 @@ class GameOfLife < Formula
   url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.42.0.tar.gz"
   sha256 "a6768bf72901dcdae1c1ed419ed35ed47be519c899b0d0bc3be5b3957e686e0e"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "969a40733ad22f0f2550c02f55804cf3cef87e364aa9c24cfc7f092051545154" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
