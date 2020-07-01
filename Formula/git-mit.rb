3660609247db5eb02b1e4d42aa19c220e117ef68
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-07-01 10:19:16 +0200

    Update git-mit to v3.67.0 (#375)
    Co-authored-by: runner <runner@Mac-1856.local>

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -3,6 +3,13 @@ class GitMit < Formula
   homepage "https://github.com/PurpleBooth/git-mit"
   url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.67.0.tar.gz"
   sha256 "17bf3ad2e0c850895a9c0d7c70b063fca9da6b3feb96108622573092b5031572"
+
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any
+    sha256 "deaf132251fea5cdb084da14936fa8c43258f8ac4db463e463d46691269c8e0a" => :catalina
+  end
+
   depends_on "pandoc" => :build
   depends_on "rust" => :build
   depends_on "git"
