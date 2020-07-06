f740e83c57c0fdfdaf31229a1f54aacfc621ee43
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-06 05:47:10 +0000

    git-duet-wrap-for-sops: update 0.41.0 bottle.

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -4,6 +4,12 @@ class GitDuetWrapForSops < Formula
   url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.41.0.tar.gz"
   sha256 "de5d4ca45bbb0f5550dde008b71df1ec47139e987f68be007d6adc2f16833c28"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "2e7c29f7bc47d8dfab487158d565e0c1073f2ee05d0b0155c29655aafda951dd" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
