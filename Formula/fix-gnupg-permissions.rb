312459410882f138053e3c9a71b273f92ba43c1b
runner <runner@Mac-1944.local>
2020-07-07 05:18:16 +0000

    Update fix-gnupg-permissions to v0.45.0

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -1,14 +1,8 @@
 class FixGnupgPermissions < Formula
   desc "Fixes permissions problems on the gnupg directory"
   homepage "https://github.com/PurpleBooth/fix-gnupg-permissions"
-  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.44.0.tar.gz"
-  sha256 "0541f68646876b3dd04052c9aa2b76620e0c8e1ba042792b0db46ecfb631170d"
-
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "0b3fbe67c8a712fbd67e7cdcbd3edbca6e8a81553855d48600cecf3475e291e3" => :catalina
-  end
+  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.45.0.tar.gz"
+  sha256 "6f6847f53a62e33a6697fc25c6a50cfcc081b02e3b14f8a4aedd2a7a89d42f46"
 
   depends_on "rust" => :build
 
