04d807ef00a62a9bf8dee28c1700b9fbf0609588
runner <runner@Mac-1897.local>
2020-07-07 05:21:43 +0000

    Update game-of-life to v1.45.0

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -1,14 +1,8 @@
 class GameOfLife < Formula
   desc "PurpleBooth's implementation of Conway's Game of life"
   homepage "https://github.com/PurpleBooth/game-of-life"
-  url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.44.0.tar.gz"
-  sha256 "403fe0c4ea0b061b1186b2fc6167f62d2a74db6e01cf18248477de2046c4344b"
-
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "b9969c687aedc09457563b37407b234da2d7e845dd997f2fa22f74303cc3130c" => :catalina
-  end
+  url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.45.0.tar.gz"
+  sha256 "6a1eca518da0092923866819b9f93b1b29e3bee14004c3c3c4cd1d2b5845a3b1"
 
   depends_on "rust" => :build
 
