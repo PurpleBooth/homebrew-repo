a896d543bb2c3371a4df7f0c16ed76ca965d196d
runner <runner@Mac-1559.local>
2020-07-08 05:29:44 +0000

    Update git-duet-wrap-for-sops to v0.43.0

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -1,8 +1,8 @@
 class GitDuetWrapForSops < Formula
   desc "Keeps your git authors file encrypted"
   homepage "https://github.com/PurpleBooth/git-duet-wrap-for-sops"
-  url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.42.0.tar.gz"
-  sha256 "48edfd58e21fe180eababbbaa567cfcac9fb5bc8fa1425a8a6ff72692824a925"
+  url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.43.0.tar.gz"
+  sha256 "6c4794a41d21c92fb41357ac2d39a5f9176f96cbfe249bca67906daef2f7c00f"
 
   depends_on "rust" => :build
 
