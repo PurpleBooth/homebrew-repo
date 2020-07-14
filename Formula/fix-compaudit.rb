42cb8472e81e9f91b6927c62fa415df572c8989f
runner <runner@Mac-1817.local>
2020-07-14 06:24:39 +0000

    Update fix-compaudit to v0.44.0

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -1,14 +1,8 @@
 class FixCompaudit < Formula
   desc "Fixes problems reported by compuaudit"
   homepage "https://github.com/PurpleBooth/fix-compaudit"
-  url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.43.0.tar.gz"
-  sha256 "a147f7f2523065e81d9e63d47b0d190712e5dfe0f7975b2626fcf897d03f3647"
-
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "714543b20c55fe5b0b6f955b381364719a69d45aae765ce7acc73c1e30db5f68" => :catalina
-  end
+  url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.44.0.tar.gz"
+  sha256 "4e281d84f5d80653f90420dc232c98aa4b506cd917fb76bf8d7270772a7acf3d"
 
   depends_on "rust" => :build
   depends_on "zsh"
