0ccb73264ebbe6f08a981e0862efa6502d4f74b1
PurpleBooth <PurpleBooth@users.noreply.github.com>
2020-07-16 12:06:21 +0000

    fix: Update git-mit to v3.95.1

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,12 +1,12 @@
 class GitMit < Formula
   desc "My personal git commit hooks"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.93.1.tar.gz"
-  sha256 "4a2592944f05b87749df395f0a31bbc3473d093d86fdcbdd4bf80f20a920b453"
+  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v3.95.1.tar.gz"
+  sha256 "11cda3cbb82e36bd2bbd96836804babee35827a21eb53ab129719d40af1411ce"
   bottle do
     root_url "https://dl.bintray.com/purplebooth/bottles-repo"
     cellar :any
-    sha256 "421f0db129339ea8b3e2d67e90ab3953557610884003bbbef41bb707cf8f9bcb" => :catalina
+    sha256 "e65e753679aa3bf47036a263f6712ff162c3e9d19c1f730504b331b9eba2a08f" => :catalina
   end
 
   depends_on "pandoc" => :build
