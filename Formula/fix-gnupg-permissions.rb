db565889da4385e254f77145f1ac381092a614a8
PurpleBooth <PurpleBooth@users.noreply.github.com>
2020-07-19 20:51:18 +0000

    fix: Update fix-gnupg-permissions to v0.49.0

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -1,13 +1,13 @@
 class FixGnupgPermissions < Formula
   desc "Fixes permissions problems on the gnupg directory"
   homepage "https://github.com/PurpleBooth/fix-gnupg-permissions"
-  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.48.0.tar.gz"
-  sha256 "17518f92406b740acf4913cb2ee689509cea44cb418a96502ed6e013b2fb22d5"
+  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.49.0.tar.gz"
+  sha256 "0e098f3d75416e7a96db2a529166b92f6d647f3aa16458bbcc92df627fb93fa3"
 
   bottle do
     root_url "https://dl.bintray.com/purplebooth/bottles-repo"
     cellar :any_skip_relocation
-    sha256 "117bcff1717e87a64a5d58de95d9f2bf3b2869e166697b79ec40c85088431ffb" => :catalina
+    sha256 "34762e219fb2e9abaf76f2295f4dcd8c55536264363e709ab6829b1c1574ac5e" => :catalina
   end
 
   depends_on "rust" => :build
