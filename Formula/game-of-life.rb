4fb5e0a806a08c11af195d128e0b158cf0f7d802
PurpleBooth <PurpleBooth@users.noreply.github.com>
2020-07-25 08:47:11 +0000

    fix: Update game-of-life to v1.53.0

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -1,13 +1,13 @@
 class GameOfLife < Formula
   desc "PurpleBooth's implementation of Conway's Game of life"
   homepage "https://github.com/PurpleBooth/game-of-life"
-  url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.52.0.tar.gz"
-  sha256 "07029de88d56181886dcbb4fb2cc883f9cf1b36ec83d9be6f702d56ecb6ae6c5"
+  url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.53.0.tar.gz"
+  sha256 "7b322ea5b4fe20c948e7aaa6ec052db82f3f96f84fa6ee0a84195304c3485e77"
 
   bottle do
     root_url "https://dl.bintray.com/purplebooth/bottles-repo"
     cellar :any_skip_relocation
-    sha256 "93dd0f590ac0f45bf77921fbb62bd7fb3e8c7e895f0696a024c078c5f0bc98c9" => :catalina
+    sha256 "35a6c316faba0f020c3e2b740daaee2bb1a65cc31244882d50fc9ac6b2087736" => :catalina
   end
 
   depends_on "rust" => :build
@@ -19,5 +19,6 @@ class GameOfLife < Formula
   test do
     system "#{bin}/game-of-life", "-h"
     system "#{bin}/game-of-life", "-V"
+    system "#{bin}/game-of-life", "-s", "1"
   end
 end
