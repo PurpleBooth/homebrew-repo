7090dc2c791dd7cd543b458299d75b8a6732986a
PurpleBooth <PurpleBooth@users.noreply.github.com>
2020-08-12 07:36:37 +0000

    art-station-i-guess: update 0.80.9 bottle.

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -1,16 +1,19 @@
 class ArtStationIGuess < Formula
   desc "Download art from Art Station for wallpaper"
   homepage "https://github.com/PurpleBooth/art-station-i-guess"
-  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.80.5.tar.gz"
-  sha256 "e5f78343fc1356cbcb7613462cd6b0dbbf86655fbf1d3bd61585c0df5241c8f7"
+  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.80.9.tar.gz"
+  sha256 "603788f6765fd89630964b2ea5be8e0ba6b92bb72e57d9fda8387ead62076384"
 
   bottle do
     root_url "https://dl.bintray.com/purplebooth/bottles-repo"
     cellar :any_skip_relocation
-    sha256 "ac25ce8a886e32dd761400720481729e580ccae5481f62d8fbbb5ca9bfefb2ac" => :catalina
+    sha256 "4b42f061c18dac71c25dbee1da47e4f1a16e24df1c8c8f008eb5fea6bd283855" => :catalina
   end
 
   depends_on "rust" => :build
+  on_linux do
+    depends_on "openssl@1.1"
+  end
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
