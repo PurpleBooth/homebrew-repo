d18d8761ecb6b5e2ff4f85547ee5f2b48d54c11d
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-08-22 11:23:17 +0000

    fix: Added new bottles for git-duet-wrap-for-sops macOS-latest

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -4,6 +4,12 @@ class GitDuetWrapForSops < Formula
   url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.46.6.tar.gz"
   sha256 "e890187c09d88a9b79ab42cdab111e6c6820b96ee10a1417ffbdb4e5266ddb50"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "2e329f73d62522f1b51c55d4152aaf7be407f777a31ce691ef3084ddd9a51cd3" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
