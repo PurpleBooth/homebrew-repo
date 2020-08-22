f81330b5a7a20b4fb01fa18f667a44ec76b9a64b
Billie Thompson <billie@billiecodes.com>
2020-08-22 15:10:43 +0200

    fix: Remove bottles
    These are currently broken

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -4,12 +4,6 @@ class FixCompaudit < Formula
   url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.46.44.tar.gz"
   sha256 "90f3041839e05c68b1cfc1e2a243aaa5d30d786a21d3dd67b80ef62e59362ad3"
 
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "bb869359dafd75ef3de1979de3f74af1bea2860c0e69681a3fe9cc51a01d4832" => :catalina
-  end
-
   depends_on "rust" => :build
   depends_on "zsh"
 
