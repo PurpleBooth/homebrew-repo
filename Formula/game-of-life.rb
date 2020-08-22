66b537b0b1cb832b1813152b9e3bcaefd3ac3f7c
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-08-22 11:50:39 +0000

    fix: Added new bottles for game-of-life ubuntu-latest

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -4,6 +4,12 @@ class GameOfLife < Formula
   url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.53.11.tar.gz"
   sha256 "f2ec3d77db5d50c864892f4824c86d4e410209e76f2146a9991263bffd9295ad"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "8b69cba60ebb2f4871bf4db96ba046e4c51040816556c1d2107244b1883a317a" => :x86_64_linux
+  end
+
   depends_on "rust" => :build
 
   def install
