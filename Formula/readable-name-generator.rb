bd95f112ea1c9d83caea02bb156dbddcd2f56209
Billie Thompson <billie@billiecodes.com>
2020-08-22 11:38:31 +0200

    fix: Remove existing bottles

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -4,12 +4,6 @@ class ReadableNameGenerator < Formula
   url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v2.68.0.tar.gz"
   sha256 "d6900c7e235b91c1df21bbf2b91a0793df5025baf1b3a246a7e1459681912322"
 
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "34a0e9a72256665c618ede50110fd77d68c4183ad387ed4fbb9fd6fb55a72570" => :catalina
-  end
-
   depends_on "go" => :build
 
   def install
