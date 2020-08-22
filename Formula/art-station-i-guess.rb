b75d11c3a006c97bc012fda3281aac91e5c20b1d
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-08-22 09:45:52 +0000

    fix: Added new bottles

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -4,6 +4,12 @@ class ArtStationIGuess < Formula
   url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.80.12.tar.gz"
   sha256 "bd79e5cc6f89dfb786f1f1ae32753afc3e0f2286c79a826ec1b531cd0674ea65"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "9b3873d317d708f9e47a2128c0a015fccd6fbeff50606842ac80dcf5b0b67565" => :x86_64_linux
+  end
+
   depends_on "rust" => :build
   on_linux do
     depends_on "openssl@1.1"
