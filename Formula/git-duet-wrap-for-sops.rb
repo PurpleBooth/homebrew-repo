f9a84d8c47ad6c3f2d6a9a3928a69babc46ac8b9
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-08-22 10:10:45 +0000

    fix: Added new bottles for git-duet-wrap-for-sops ubuntu-latest

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -4,6 +4,12 @@ class GitDuetWrapForSops < Formula
   url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.46.6.tar.gz"
   sha256 "e890187c09d88a9b79ab42cdab111e6c6820b96ee10a1417ffbdb4e5266ddb50"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "aa40dfca0bc4059cbaea3e6513229073ed1341633fc7a93c58fd72b952a351b2" => :x86_64_linux
+  end
+
   depends_on "rust" => :build
 
   def install
