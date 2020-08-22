7898fc1c17fbb7e9e67f734d7597a309585613e1
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-08-22 11:22:59 +0000

    fix: Added new bottles for game-of-life macOS-latest

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -4,6 +4,12 @@ class GameOfLife < Formula
   url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.53.11.tar.gz"
   sha256 "f2ec3d77db5d50c864892f4824c86d4e410209e76f2146a9991263bffd9295ad"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "ca28bfb6b982e6c2b1f341f5dfa3f1b48a97389d8277c8e7fb4cc6fc1e27ac35" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
