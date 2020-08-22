6e9955c4f2c6c6ac6e2be80739bbd6f3f6877ac3
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-08-22 11:22:08 +0000

    fix: Added new bottles for fix-gnupg-permissions macOS-latest

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -4,6 +4,12 @@ class FixGnupgPermissions < Formula
   url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.49.12.tar.gz"
   sha256 "c7d06361dce24e7697d872d639479fdea43b6708a4d77b75ba4adcdd685be668"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "ffb0ff8896574c39191b19b89aa1db67318832a9c9b713acbf4e965ac7b4dc6e" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
