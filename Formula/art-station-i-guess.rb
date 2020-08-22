4a1d46c3ef912fd58bf1f6729b0ff7f96339b042
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-08-22 10:07:20 +0000

    fix: Added new bottles for art-station-i-guess macOS-latest

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -4,6 +4,12 @@ class ArtStationIGuess < Formula
   url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.80.12.tar.gz"
   sha256 "bd79e5cc6f89dfb786f1f1ae32753afc3e0f2286c79a826ec1b531cd0674ea65"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "9b55b75effbbabe16118c550dfbee6339d9caa6f051a2b79feacec415818bf76" => :catalina
+  end
+
   depends_on "rust" => :build
   on_linux do
     depends_on "openssl@1.1"
