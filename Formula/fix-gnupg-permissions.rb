6db4e0eb438ea45c325354daff6cb646d86ed692
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-08-22 10:09:39 +0000

    fix: Added new bottles for fix-gnupg-permissions ubuntu-latest

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -4,6 +4,12 @@ class FixGnupgPermissions < Formula
   url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.49.12.tar.gz"
   sha256 "c7d06361dce24e7697d872d639479fdea43b6708a4d77b75ba4adcdd685be668"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "cf60c3ed7d4957c127de14bbd92525c75ca7630e4b5c2567927ea448273d6a1c" => :x86_64_linux
+  end
+
   depends_on "rust" => :build
 
   def install
