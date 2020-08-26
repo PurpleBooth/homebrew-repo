5898cc7ce9f6ab0ed5e691405012d3a18ea5c0d0
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-08-26 06:11:13 +0000

    fix: Update log-light-state to v1.0.26

--- Formula/log-light-state.rb
+++ Formula/log-light-state.rb
@@ -1,8 +1,9 @@
 class LogLightState < Formula
   desc "Log the current state of hue lights every 60 seconds"
   homepage "https://github.com/PurpleBooth/log-light-state"
-  url "https://github.com/PurpleBooth/log-light-state/archive/refs/tags/v1.0.25.tar.gz"
-  sha256 "70690772bd0ad71bd3a2d4ab33e67fa6e86d61a2fece4bbb470c0b8c1f89195f"
+  url "https://github.com/PurpleBooth/log-light-state/archive/refs/tags/v1.0.26.tar.gz"
+  sha256 "83d6d5d726d3a5dc3500605ba744427946eb034ceb3e8f8b25ed8fccc5e6860b"
+  license "CC0-1.0"
   depends_on "rust" => :build
   on_linux do
     depends_on "openssl@1.1"
