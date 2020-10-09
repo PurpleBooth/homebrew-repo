b39f01e4a728751367b22e816f4ee5f093405e77
PurpleBooth <PurpleBooth@users.noreply.github.com>
2020-10-09 07:50:50 +0000

    fix: Update fix-compaudit to v0.46.48

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -1,8 +1,8 @@
 class FixCompaudit < Formula
   desc "Fixes problems reported by compuaudit"
   homepage "https://github.com/PurpleBooth/fix-compaudit"
-  url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.46.47.tar.gz"
-  sha256 "b037ae90b2f024aa0471fc10bdaef0ccf19cd3d983ac64f76f87646362016ee0"
+  url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.46.48.tar.gz"
+  sha256 "b4c94c08ab13b0aaae45e4d7c3ae96ff54c19222d2796225432c6d64c8300c41"
 
   depends_on "rust" => :build
   depends_on "zsh"
