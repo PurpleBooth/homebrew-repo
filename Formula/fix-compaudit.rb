f4b7de334ab5081a96ce1389e3d1b4df9195177b
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-10-14 07:29:07 +0000

    fix: Added missing bottles for purplebooth/repo/fix-compaudit v0.46.47

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -4,6 +4,12 @@ class FixCompaudit < Formula
   url "https://github.com/PurpleBooth/fix-compaudit/archive/refs/tags/v0.46.47.tar.gz"
   sha256 "b037ae90b2f024aa0471fc10bdaef0ccf19cd3d983ac64f76f87646362016ee0"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "e7cfd0146dc63e17b3e90ebfd0bc61d07931e20e493b4e3cf292314a59f1181a" => :x86_64_linux
+  end
+
   depends_on "rust" => :build
   depends_on "zsh"
 
