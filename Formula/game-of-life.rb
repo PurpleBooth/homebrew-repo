fb5139735b49db1367a9e1e6adc1a2599328ab61
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-10-14 07:29:15 +0000

    fix: Added missing bottles for purplebooth/repo/game-of-life v1.53.15

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -4,6 +4,12 @@ class GameOfLife < Formula
   url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.53.15.tar.gz"
   sha256 "e1ca869c8595a1cf5c5528353f13d0d12e0fb0f10bf2630e8588bdfe3ef65cd5"
 
+  bottle do
+    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
+    cellar :any_skip_relocation
+    sha256 "2778471a75cbb1d52f0876ddaa014d67d54997831ad5f5e048b191161bc6d299" => :x86_64_linux
+  end
+
   depends_on "rust" => :build
 
   def install
