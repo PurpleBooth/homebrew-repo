96b843518ea476971759ce253337a07cf096f0e0
PurpleBooth <PurpleBooth@users.noreply.github.com>
2020-12-21 05:50:47 +0000

    fix: Update game-of-life to v1.53.25

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -1,8 +1,8 @@
 class GameOfLife < Formula
   desc "PurpleBooth's implementation of Conway's Game of life"
   homepage "https://github.com/PurpleBooth/game-of-life"
-  url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.53.24.tar.gz"
-  sha256 "f9969178bc7dd60a4ebbe1d987fdf2de8ab075927026f4088f153117d56f9a8f"
+  url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.53.25.tar.gz"
+  sha256 "c61e0ef649d78b7acb8565bc71b8f3f0fbff9c4fd7b4df959209b833ad596cc3"
 
   depends_on "rust" => :build
 
