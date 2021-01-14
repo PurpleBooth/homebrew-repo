2d1002a8600a93f84539e5fedd2b3afba1a89f58
PurpleBooth <PurpleBooth@users.noreply.github.com>
2021-01-14 07:13:13 +0000

    fix: Update fix-gnupg-permissions to v0.49.25

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -1,8 +1,8 @@
 class FixGnupgPermissions < Formula
   desc "Fixes permissions problems on the gnupg directory"
   homepage "https://github.com/PurpleBooth/fix-gnupg-permissions"
-  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.49.24.tar.gz"
-  sha256 "0bd3292d26346f787a6e447d72dc9c8f591ba6e2d4d308787297b3ffd6bded93"
+  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.49.25.tar.gz"
+  sha256 "44de8a0d048ff7d3ce648eb3e9ee5da33bf77f93b0d2e493f2a5c0d82bf0a8b3"
   license "CC0-1.0"
 
   depends_on "rust" => :build
