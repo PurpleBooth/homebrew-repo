f729c17d4274fdf51c99b19179be2c9525841c2c
PurpleBooth <PurpleBooth@users.noreply.github.com>
2021-03-09 05:16:43 +0000

    fix: Update art-station-i-guess to v0.80.49

--- Formula/art-station-i-guess.rb
+++ Formula/art-station-i-guess.rb
@@ -1,8 +1,8 @@
 class ArtStationIGuess < Formula
   desc "Download art from Art Station for wallpaper"
   homepage "https://github.com/PurpleBooth/art-station-i-guess"
-  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.80.46.tar.gz"
-  sha256 "ceeb265fa1a9c6768373f415531e58f614068e85ca787b9e552af5b821e0fd2d"
+  url "https://github.com/PurpleBooth/art-station-i-guess/archive/refs/tags/v0.80.49.tar.gz"
+  sha256 "41719d27d12b4c124f5d86208cc2db984e602e7de47c0551e3cea6f7f1eeead5"
 
   depends_on "rust" => :build
   on_linux do
