97a3e861f4680dadd8c321786cd60bc93d210f60
PurpleBooth <PurpleBooth@users.noreply.github.com>
2021-03-12 05:14:29 +0000

    fix: Update readable-name-generator to v2.95.0

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -1,8 +1,8 @@
 class ReadableNameGenerator < Formula
   desc "Generate a readable name using Dockers formula"
   homepage "https://github.com/PurpleBooth/readable-name-generator"
-  url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v2.91.0.tar.gz"
-  sha256 "7846cca8b329d77f076e1712ffdabed37d3455a4016b22b240174c9f5ed0c512"
+  url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v2.95.0.tar.gz"
+  sha256 "f81a3ff16d11f8f7ba78fa4f7d9d5a984dbcf61fac715d6bad62a4d0cd41389f"
 
   depends_on "go" => :build
 
