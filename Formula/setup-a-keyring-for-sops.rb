7ba36837158c1b98c0a3452c721bc10cb2395d38
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-03-31 21:50:23 +0200

    fix: Remove unused bottle

--- Formula/setup-a-keyring-for-sops.rb
+++ Formula/setup-a-keyring-for-sops.rb
@@ -4,12 +4,6 @@ class SetupAKeyringForSops < Formula
   url "https://github.com/PurpleBooth/setup-a-keyring-for-sops/archive/refs/tags/v0.42.0.tar.gz"
   sha256 "470dd5f27b36d3e2b740613d670b69744e1af49ecf0935ddf160816187bd614d"
 
-  bottle do
-    root_url "https://dl.bintray.com/purplebooth/bottles-repo"
-    cellar :any_skip_relocation
-    sha256 "aa7c971f92f8eb6ddd2939cb657507dc22122b2476e2ccd92cfe8d81d7f4d045" => :x86_64_linux
-  end
-
   depends_on "rust" => :build
 
   def install
