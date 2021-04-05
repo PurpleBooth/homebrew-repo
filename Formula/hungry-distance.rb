7086fa0b4670d01af61d859dc98799541e7bf856
PurpleBooth <PurpleBooth@users.noreply.github.com>
2021-04-05 19:13:16 +0000

    fix: Update hungry-distance to v0.1.2

---
+++ Formula/hungry-distance.rb
@@ -0,0 +1,18 @@
+class HungryDistance < Formula
+  desc "Calculate the distance between two points in an XYZ space"
+  homepage "https://github.com/PurpleBooth/hungry-distance"
+  url "https://github.com/PurpleBooth/hungry-distance/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "0cecc2c4836a7050d7a0ae791c31cf5250b50ed2f91a667eb7dc664714bfc988"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/hungry-distance", "-h"
+    system "#{bin}/hungry-distance", "-V"
+    system "#{bin}/hungry-distance", "--", "-11.46875", "39.78125", "22.78125", "73.875", "-3.5625", "-52.625"
+  end
+end
