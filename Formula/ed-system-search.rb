bdcd045158d3568865433df4defde10ca79d9aa7
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-04-08 07:03:51 +0000

    fix: Update ed-system-search to v0.1.0
    Closes #885.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

---
+++ Formula/ed-system-search.rb
@@ -0,0 +1,18 @@
+class EdSystemSearch < Formula
+  desc "Find interesting systems in Elite: Dangerous"
+  homepage "https://github.com/PurpleBooth/ed-system-search"
+  url "https://github.com/PurpleBooth/ed-system-search/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "94e724ab327290795c4d0b6d07bfd3fd4a3b6451683041b51a66c25b7aad5763"
+
+  depends_on "rust" => :build
+  depends_on "zsh"
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/ed-system-search", "-h"
+    system "#{bin}/ed-system-search", "-V"
+  end
+end
