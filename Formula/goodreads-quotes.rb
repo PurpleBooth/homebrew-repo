28b9e2f0f196ea9a7286a8d202d6d366433aee22
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-04-13 05:34:27 +0000

    fix: Update goodreads-quotes to v0.86.31
    Closes #909.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/goodreads-quotes.rb
+++ Formula/goodreads-quotes.rb
@@ -1,14 +1,8 @@
 class GoodreadsQuotes < Formula
   desc "Get a random quote from random categories in goodreads"
   homepage "https://github.com/PurpleBooth/goodreads-quotes"
-  url "https://github.com/PurpleBooth/goodreads-quotes/archive/refs/tags/v0.86.30.tar.gz"
-  sha256 "96257205c5078af9862aa12b2ec73905a2910aaf97ccc68e7ea20587621f3007"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/goodreads-quotes-0.86.30"
-    sha256 cellar: :any_skip_relocation, catalina:     "6f5c5cc755fa10f4d0d69f818a31122988235a53ecf7c8246708ef6ff4a3d395"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c4bb7281dd9a6ff7a2402dd5dc04c0f8c63d0d3ac0eeec1a2678d319d792e1d2"
-  end
+  url "https://github.com/PurpleBooth/goodreads-quotes/archive/refs/tags/v0.86.31.tar.gz"
+  sha256 "554c958feaf19793c1cf3e89a3e0a909c74e31f5c15d1b112e0889fdb95ba2be"
 
   depends_on "rust" => :build
   on_linux do
