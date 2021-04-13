1fb59ab315bb7f5ea2eb5869c1b31aa44eb162e0
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-04-13 05:48:05 +0000

    git-duet-wrap-for-sops: add 0.46.20 bottle.

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -4,6 +4,12 @@ class GitDuetWrapForSops < Formula
   url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.46.20.tar.gz"
   sha256 "e27960ed8c46c129477eb01586db2018357ca08af874e87606a7c7685b0bd89e"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-duet-wrap-for-sops-0.46.20"
+    sha256 cellar: :any_skip_relocation, catalina:     "3e36c412ddd27ebcae6adcdafb05081a4c97b9aa72eac6f4922fbf3d48c51e45"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b9521842d3444f1448856d6c12ca2b0932069a42433d8f0f8a94511441a55761"
+  end
+
   depends_on "rust" => :build
 
   def install
