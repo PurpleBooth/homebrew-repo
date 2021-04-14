d58d6fca3dd0f8878ab8319db6cfa2532633e793
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-04-14 20:24:39 +0000

    fix: Update fix-gnupg-permissions to v0.49.31

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -1,15 +1,9 @@
 class FixGnupgPermissions < Formula
   desc "Fixes permissions problems on the gnupg directory"
   homepage "https://github.com/PurpleBooth/fix-gnupg-permissions"
-  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.49.30.tar.gz"
-  sha256 "7e55a2004b5ee93b57120473d231336fd0428be88b6c3beeda42f7dd43a2d103"
+  url "https://github.com/PurpleBooth/fix-gnupg-permissions/archive/refs/tags/v0.49.31.tar.gz"
+  sha256 "7679ececc0bcd6e12cb25eae5208d538b338be1d5e70bffbf84844e24a605e24"
   license "CC0-1.0"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/fix-gnupg-permissions-0.49.30"
-    sha256 cellar: :any_skip_relocation, catalina:     "b747206f3b63bd8e4cc12c4700ba7100a55d481a38098ff7ab58c2865c25cd3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6a38e3fdb3cb0bd2acf2e647de450877dfcdac7ece4d9e713a12477a219dd7e7"
-  end
 
   depends_on "rust" => :build
 
