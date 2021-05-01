2893e418eb0223135237689009f913d10d95e321
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-05-01 10:11:04 +0000

    fix: Update ellipsis to v0.1.0

---
+++ Formula/ellipsis.rb
@@ -0,0 +1,17 @@
+class Ellipsis < Formula
+  desc "Manage and provision dotfiles"
+  homepage "https://github.com/PurpleBooth/ellipsis"
+  url "https://github.com/PurpleBooth/ellipsis/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "4de246a920c7db27c2387e4638c131aae6e6f2169bc301cd1729fac5406e1301"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/ellipsis", "-h"
+    system "#{bin}/ellipsis", "-V"
+  end
+end
