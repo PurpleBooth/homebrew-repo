a3860608b128140a4fa9e979374b20ccec4638b9
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-05-13 07:09:20 +0000

    fix: Update ellipsis to v0.5.3
    Closes #964.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/ellipsis.rb
+++ Formula/ellipsis.rb
@@ -1,14 +1,8 @@
 class Ellipsis < Formula
   desc "Manage and provision dotfiles"
   homepage "https://github.com/PurpleBooth/ellipsis"
-  url "https://github.com/PurpleBooth/ellipsis/archive/refs/tags/v0.5.2.tar.gz"
-  sha256 "009e666c87296ae69faa9f8af06f36bfced95f2cff099d0ff1262cbb639e3e6c"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/ellipsis-0.5.2"
-    sha256 cellar: :any_skip_relocation, catalina:     "d509ab6d2e52edd9a05d13882fefcc478e13761e8ddacbcce91fcc54b57cb2b9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e8de04685028e9c64f4f28ba46128fa58a9f98fba9693b451bef77b3737a336b"
-  end
+  url "https://github.com/PurpleBooth/ellipsis/archive/refs/tags/v0.5.3.tar.gz"
+  sha256 "9ff54dab386036b1fc5af597f5e95b4e567870adb308806e53b1d71d4766ce25"
 
   depends_on "rust" => :build
 
