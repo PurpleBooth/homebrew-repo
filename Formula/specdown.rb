2826f75faa2b1e77b6e6cad1ed3833b0f01d6b19
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-06-05 09:27:50 +0000

    specdown: add 0.43.0 bottle.

--- Formula/specdown.rb
+++ Formula/specdown.rb
@@ -4,6 +4,12 @@ class Specdown < Formula
   url "https://github.com/specdown/specdown/archive/refs/tags/v0.43.0.tar.gz"
   sha256 "8993108d74640dc0cb2418832ec0f9f677d652bfb9d19959fb97499ad1cfb5a3"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/specdown-0.43.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "fbbc4eaceeadf79f05a8d7ffe9c9b73892f0830ac376b3f62e610d79d3d51263"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6cb3b41a8a23a5b3da26f0553205ab2add08b35ffaad393fa6d6e8065a7d5399"
+  end
+
   depends_on "rust" => :build
 
   resource("testdata") do
