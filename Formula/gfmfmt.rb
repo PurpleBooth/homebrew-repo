0732e4fa384041cae04cb42d9d431ecb11ddb253
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-06-15 19:37:40 +0000

    gfmfmt: add 0.1.2 bottle.

--- Formula/gfmfmt.rb
+++ Formula/gfmfmt.rb
@@ -3,6 +3,12 @@ class Gfmfmt < Formula
   homepage "https://github.com/PurpleBooth/gfmfmt"
   url "https://github.com/PurpleBooth/gfmfmt/archive/v0.1.2.tar.gz"
   sha256 "73bec4420f82d64547532281c3aa72b0ee8d2ede824c61a08624c7b1979e299c"
+
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/gfmfmt-0.1.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "f129f7a7aee9e8a745c3e216ab02cb493db0395157825846250b409388db57c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6885db47e1cf69c700e9ee16b5e40f4cffc159eab51ed85ab22eb4664f568942"
+  end
   depends_on "bash"
   depends_on "pandoc"
 
