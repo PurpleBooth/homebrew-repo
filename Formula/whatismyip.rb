ec4158bba790861263c7197702950d803067490b
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-06-17 10:14:41 +0000

    fix: Update whatismyip to v0.6.3
    Closes #995.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

---
+++ Formula/whatismyip.rb
@@ -0,0 +1,17 @@
+class Whatismyip < Formula
+  desc "Manage and provision dotfiles"
+  homepage "https://github.com/PurpleBooth/whatismyip"
+  url "https://github.com/PurpleBooth/whatismyip/archive/v0.6.3.tar.gz"
+  sha256 "b0d2732230895aea42e439a20f62d858a0508a00e16894c5e573efa33603e633"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/whatismyip", "-h"
+    system "#{bin}/whatismyip", "-V"
+  end
+end
