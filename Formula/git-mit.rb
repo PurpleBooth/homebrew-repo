b916d236adec3cfccf893744e3a7873849924fd4
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-08-13 08:02:08 +0000

    fix: Update git-mit to v4.0.13
    Closes #1032.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,20 +1,15 @@
 class GitMit < Formula
   desc "Minimalist set of hooks to aid pairing and link commits to issues"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/v4.0.11.tar.gz"
-  sha256 "f5d27e0606b141096fcf473d6e57674ac0bd8fd1516fdf70e54e97208e94d5e1"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-mit-4.0.11"
-    sha256 cellar: :any,                 catalina:     "784fc90fd489169632ff1b9d5a175b5a3b4ad9513b62b476aba43c31b250fa80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f3e13d56395be678206f3be2def3b0dccc4ab776076de5a0f5de920ca5b3fbe3"
-  end
+  url "https://github.com/PurpleBooth/git-mit/archive/v4.0.13.tar.gz"
+  sha256 "453f6fa58d8848ca2aa6418616c981d1a7be26de59c767b162389700aa84dd74"
   depends_on "pandoc" => :build
   depends_on "rust" => :build
   depends_on "git"
   depends_on "openssl@1.1"
   on_linux do
     depends_on "libxcb"
+    depends_on "libxkbcommon"
   end
 
   def install
