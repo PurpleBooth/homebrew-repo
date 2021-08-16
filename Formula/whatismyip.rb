6a914b544b7e26718bffc799405f0808030cb237
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-08-16 18:26:28 +0000

    fix: Update whatismyip to v0.9.9
    Closes #1040.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/whatismyip.rb
+++ Formula/whatismyip.rb
@@ -1,14 +1,8 @@
 class Whatismyip < Formula
   desc "Manage and provision dotfiles"
   homepage "https://github.com/PurpleBooth/whatismyip"
-  url "https://github.com/PurpleBooth/whatismyip/archive/v0.9.8.tar.gz"
-  sha256 "12131bbdcc18ca46c6ffa6d4f0f280359b6d3632e91744fa03b8d5375533dd62"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/whatismyip-0.9.8"
-    sha256 cellar: :any_skip_relocation, catalina:     "a2b356931869873f591920bef09198c3d899543b93b12d195641fe7046352bdf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2f0bb3ff49327067137713bed97e79c5bfa399037d4053a77a0388dcc82315ef"
-  end
+  url "https://github.com/PurpleBooth/whatismyip/archive/v0.9.9.tar.gz"
+  sha256 "b715d7dace23c27ffa08dbd4dc33d3b13157e645db0fc2bf4c45d4e95c713c51"
 
   depends_on "rust" => :build
 
