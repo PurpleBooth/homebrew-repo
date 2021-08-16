a5161ad6695e43d48d90c6e448afa9eef79e9602
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-08-16 06:30:09 +0000

    fix: Update ed-system-search to v1.0.14

--- Formula/ed-system-search.rb
+++ Formula/ed-system-search.rb
@@ -1,14 +1,8 @@
 class EdSystemSearch < Formula
   desc "Find interesting systems in Elite: Dangerous"
   homepage "https://github.com/PurpleBooth/ed-system-search"
-  url "https://github.com/PurpleBooth/ed-system-search/archive/v1.0.13.tar.gz"
-  sha256 "8c41f7fa1687fabb3c8c271cb7eae6f7f2226609b19822848a332b746f3eb55d"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/ed-system-search-1.0.13"
-    sha256 cellar: :any_skip_relocation, catalina:     "d9c6c6ab4956829dd9ad239dc9d6d34607d8554a3e7dc4e7b342927daceda5fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "db7a09f057303975f11a0e039624ba8049c1f5a86a356637fae3ebd8389aca38"
-  end
+  url "https://github.com/PurpleBooth/ed-system-search/archive/v1.0.14.tar.gz"
+  sha256 "342d42e838ffacc7821efdf977baa911eb28138ba8e80c74bb06cf4e14293d97"
 
   depends_on "rust" => :build
 
