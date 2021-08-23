fe10c5c07b02a373f5d8b30f7639b2d8bef54157
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-08-23 21:28:37 +0000

    ellipsis: add 0.6.10 bottle.

--- Formula/ellipsis.rb
+++ Formula/ellipsis.rb
@@ -4,6 +4,12 @@ class Ellipsis < Formula
   url "https://github.com/PurpleBooth/ellipsis/archive/v0.6.10.tar.gz"
   sha256 "557e80768a31b3fe51b986dea819980cf11afdc81579e48cb8ff357ba7bfa980"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/ellipsis-0.6.10"
+    sha256 cellar: :any_skip_relocation, catalina:     "8491c7b1fdff7797506293cc68e1215b0bef5ce53debac860db68d724ccdce13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0d3bc36e9d4156ba1f2e5fabf39dc97b578d570f55b5d7c74ffd40fbd99a7c75"
+  end
+
   depends_on "rust" => :build
 
   def install
