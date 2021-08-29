4d1a75ce27e42b38456739a36b7a27011a438652
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-08-29 21:11:23 +0000

    fix: Update git-moves-together to v0.1.3

---
+++ Formula/git-moves-together.rb
@@ -0,0 +1,17 @@
+class GitMovesTogether < Formula
+  desc "Find coupling in git repositories"
+  homepage "https://github.com/PurpleBooth/git-moves-together"
+  url "https://github.com/PurpleBooth/git-moves-together/archive/v0.1.3.tar.gz"
+  sha256 "d2fd9ef1c260f8757bab8813aaae21579d9c3638046ae6aa835efd3e3de39280"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "git", "clone", "https://github.com/PurpleBooth/git-moves-together.git"
+    system "#{bin}/git-moves-together", "git-moves-together"
+  end
+end
