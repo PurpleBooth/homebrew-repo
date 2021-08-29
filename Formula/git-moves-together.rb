4994856e552a19e4b2d3c90ff2c44d32c4e6433a
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-08-29 21:43:57 +0000

    fix: Update git-moves-together to v0.1.5
    Closes #1064.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

---
+++ Formula/git-moves-together.rb
@@ -0,0 +1,21 @@
+class GitMovesTogether < Formula
+  desc "Find coupling in git repositories"
+  homepage "https://github.com/PurpleBooth/git-moves-together"
+  url "https://github.com/PurpleBooth/git-moves-together/archive/v0.1.5.tar.gz"
+  sha256 "807da44ac37a76c96ac3a3aca4383de63bfaf5a7306303cc2ba1220253d19dc5"
+
+  depends_on "rust" => :build
+  depends_on "openssl@1.1"
+  on_linux do
+    depends_on "zlib"
+  end
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "git", "clone", "https://github.com/PurpleBooth/git-moves-together.git"
+    system "#{bin}/git-moves-together", "git-moves-together"
+  end
+end
