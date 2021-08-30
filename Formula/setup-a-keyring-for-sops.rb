e028be80a4579e39d4dc985fadfd7cea21b8495f
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-08-30 02:21:44 +0000

    fix: Update setup-a-keyring-for-sops to v0.42.14

--- Formula/setup-a-keyring-for-sops.rb
+++ Formula/setup-a-keyring-for-sops.rb
@@ -1,14 +1,8 @@
 class SetupAKeyringForSops < Formula
   desc "Use gcloud to setup everything needed for SOPS"
   homepage "https://github.com/PurpleBooth/setup-a-keyring-for-sops"
-  url "https://github.com/PurpleBooth/setup-a-keyring-for-sops/archive/refs/tags/v0.42.12.tar.gz"
-  sha256 "06b674ab687c040a65d60ef79f6248392b43b301eb49b72553723a05528b8293"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/setup-a-keyring-for-sops-0.42.12"
-    sha256 cellar: :any_skip_relocation, catalina:     "e2c62b5ba24a7a09773edd31bae63f65ac618306f5b5b1bd55b2dcc083119dbe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "0ccb0bc73c25d9d6511d446656979f5273c333b06d25d59600bca2b1e8d3523d"
-  end
+  url "https://github.com/PurpleBooth/setup-a-keyring-for-sops/archive/refs/tags/v0.42.14.tar.gz"
+  sha256 "1841baec0c43965950a9395714d325c4a47ff9a81bde34c5d00bd5466b233e87"
 
   depends_on "rust" => :build
 
