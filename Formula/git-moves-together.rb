dd3c11188b84a5d1019d3a781101a02adb6907a4
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-08-30 06:35:47 +0000

    fix: Update git-moves-together to v1.0.0

--- Formula/git-moves-together.rb
+++ Formula/git-moves-together.rb
@@ -1,14 +1,8 @@
 class GitMovesTogether < Formula
   desc "Find coupling in git repositories"
   homepage "https://github.com/PurpleBooth/git-moves-together"
-  url "https://github.com/PurpleBooth/git-moves-together/archive/v0.1.5.tar.gz"
-  sha256 "807da44ac37a76c96ac3a3aca4383de63bfaf5a7306303cc2ba1220253d19dc5"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-moves-together-0.1.5"
-    sha256 cellar: :any,                 catalina:     "1ccaf4fd5b5670ff935eb619494d24af39356ab9912b6e6a0098767bd03406fd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a62fcf15588ca47e7d05a4e659997577c313d142201a68d9a947d973403001e4"
-  end
+  url "https://github.com/PurpleBooth/git-moves-together/archive/v1.0.0.tar.gz"
+  sha256 "c7cf2cfa7a02abe5915d14ad2f92d930a47d28a4e728ac4eaf7542aa5c1c378d"
 
   depends_on "rust" => :build
   depends_on "openssl@1.1"
