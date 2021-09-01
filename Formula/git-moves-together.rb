70dfd5fece9eae47f439788a1e36d4cf09439c73
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-09-01 10:46:10 +0000

    git-moves-together: add 2.3.1 bottle.

--- Formula/git-moves-together.rb
+++ Formula/git-moves-together.rb
@@ -4,6 +4,12 @@ class GitMovesTogether < Formula
   url "https://github.com/PurpleBooth/git-moves-together/archive/v2.3.1.tar.gz"
   sha256 "9c58111fdf556ae8a412a301ba7c9d4c92ff65ddad1e1e25b809f0f180ba4101"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-moves-together-2.3.1"
+    sha256 cellar: :any,                 catalina:     "6bad051c244e5168042a488d0deeb6cd5678649c95f6949129690f5b597dc2ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1cede7690ec650525a587291818ca5e56a8abf5271a8280d7ffc6eb0979d59ea"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
   on_linux do
