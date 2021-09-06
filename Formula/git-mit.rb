5ab3352d0e84763baab8dba525ac370f4298c048
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-09-06 21:49:51 +0000

    git-mit: add 5.0.1 bottle.

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -3,6 +3,12 @@ class GitMit < Formula
   homepage "https://github.com/PurpleBooth/git-mit"
   url "https://github.com/PurpleBooth/git-mit/archive/v5.0.1.tar.gz"
   sha256 "998bdaea8fce1df7149fe38d49571fa0ad29383bcb5c9a121bf4874cd5c4d14c"
+
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-mit-5.0.1"
+    sha256 cellar: :any,                 catalina:     "3e05bcf4c7e83ec171d5e7575d592e898270588c5aa7f735415defafd27b3edd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "32fa98c7e9ae26fdf63b05a191630a73f4e8e4e7af3c47b59d7e89cce9c28325"
+  end
   depends_on "pandoc" => :build
   depends_on "rust" => :build
   depends_on "git"
