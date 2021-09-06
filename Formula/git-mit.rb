8959600974962645872f568d7b97d06700a71b41
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-09-06 11:25:42 +0000

    fix: Update git-mit to v4.0.26

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,14 +1,8 @@
 class GitMit < Formula
   desc "Minimalist set of hooks to aid pairing and link commits to issues"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/v4.0.25.tar.gz"
-  sha256 "efddba2915273b592fc73f894d2095f2b21be934c268a943beeb39c5a60a888c"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-mit-4.0.25"
-    sha256 cellar: :any,                 catalina:     "0698a3f1e3256457943787bdf710e6658a303ccfc69ed3f0fb258d5a053e2a2a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b5427295f2dd5bb469cf04c259bac2aa09a197e31e362a9d1b580b53f8540a4e"
-  end
+  url "https://github.com/PurpleBooth/git-mit/archive/v4.0.26.tar.gz"
+  sha256 "536f3b5a695b7be07d0ea73055a23a2a04dfcd73c6003467293d2bba4b1fb12e"
   depends_on "pandoc" => :build
   depends_on "rust" => :build
   depends_on "git"
