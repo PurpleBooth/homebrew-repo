8ccab49fbd4462d8bb2a2673f74f91c3cf82b0e3
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-09-06 21:13:04 +0000

    fix: Update git-mit to v5.0.0
    Closes #1086.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,14 +1,8 @@
 class GitMit < Formula
   desc "Minimalist set of hooks to aid pairing and link commits to issues"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/v4.0.26.tar.gz"
-  sha256 "536f3b5a695b7be07d0ea73055a23a2a04dfcd73c6003467293d2bba4b1fb12e"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-mit-4.0.26"
-    sha256 cellar: :any,                 catalina:     "9c9579d8f6f39509dee7400cad63189fa30af692780a30b627220b314bf4edfa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c548c8206483f107826450ad84632638b84e9b09bed3028eff9c217eeeb471fa"
-  end
+  url "https://github.com/PurpleBooth/git-mit/archive/v5.0.0.tar.gz"
+  sha256 "26b7106ae2d58e70d7def5ed6d702cac2df8b0803625c9b167431e81752a422c"
   depends_on "pandoc" => :build
   depends_on "rust" => :build
   depends_on "git"
