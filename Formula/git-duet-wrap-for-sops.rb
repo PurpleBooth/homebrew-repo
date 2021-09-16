5808494c8ffe1df496b529788160aae8a51fa299
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-09-16 09:08:26 +0000

    fix: Update git-duet-wrap-for-sops to v0.46.22

--- Formula/git-duet-wrap-for-sops.rb
+++ Formula/git-duet-wrap-for-sops.rb
@@ -1,14 +1,8 @@
 class GitDuetWrapForSops < Formula
   desc "Keeps your git authors file encrypted"
   homepage "https://github.com/PurpleBooth/git-duet-wrap-for-sops"
-  url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.46.21.tar.gz"
-  sha256 "f78a0c99d3a35c534bf062b321b74264e96c32c3a7973ce914bd19919bb41dda"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-duet-wrap-for-sops-0.46.21"
-    sha256 cellar: :any_skip_relocation, catalina:     "91bf3ebd583e684b98687a14b0901c645214ee8e6be741cd41b4237029ee0670"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2da856856c698f14ce1791e1e41cb0bc27af23e0c1f12987e8030ee5bf811527"
-  end
+  url "https://github.com/PurpleBooth/git-duet-wrap-for-sops/archive/refs/tags/v0.46.22.tar.gz"
+  sha256 "b834535a8d28678712a47b9897a363848dcd597e473edcff74c68fa7173d8190"
 
   depends_on "rust" => :build
 
