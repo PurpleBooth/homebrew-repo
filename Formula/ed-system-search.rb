ccb26622b882ddef723ff422ab3a75c643bcee19
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-11-08 01:25:16 +0000

    ed-system-search: add 1.1.5 bottle.

--- Formula/ed-system-search.rb
+++ Formula/ed-system-search.rb
@@ -4,6 +4,12 @@ class EdSystemSearch < Formula
   url "https://github.com/PurpleBooth/ed-system-search/archive/v1.1.5.tar.gz"
   sha256 "c1ad33af8970b4e33ab5971a53561daf56ce8d09ff1ec35151acd3df3fb31d34"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/ed-system-search-1.1.5"
+    sha256 cellar: :any_skip_relocation, catalina:     "edd754c501d27cbde60e54c280e077201747c6ba9ef86b536775a5d7603278cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "750978b7e375ad84155790c0215e9ebc2eab9b732dde55b071411aa8d04b1ed4"
+  end
+
   depends_on "rust" => :build
 
   def install
