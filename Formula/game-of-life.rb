b114028e09d885545dac6d33c456243694409736
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-12-01 00:12:14 +0000

    fix: Update game-of-life to v1.53.45

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -1,14 +1,8 @@
 class GameOfLife < Formula
   desc "PurpleBooth's implementation of Conway's Game of life"
   homepage "https://github.com/PurpleBooth/game-of-life"
-  url "https://github.com/PurpleBooth/game-of-life/archive/v1.53.44.tar.gz"
-  sha256 "3f45bdf881cb69a1e9c5b042aeedceacad382967b7fc358981e2de147e7dc92b"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/game-of-life-1.53.44"
-    sha256 cellar: :any_skip_relocation, catalina:     "8034f45f0c83e303a5b352ca5b90b6be14406e0cb12a8ab20fdfed735137d66d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e6917fd26f12a103edcb0ee3adfe9eb38bc25443754f32ac12d06c00767a3715"
-  end
+  url "https://github.com/PurpleBooth/game-of-life/archive/v1.53.45.tar.gz"
+  sha256 "1c888637b50a6490defa5c052f42c74c3ddbdb259ade9fd815a2b18917b485f8"
 
   depends_on "rust" => :build
 
