80a3833c4af8a41e8c6129c69726ffef6814ebf5
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-01-03 00:04:03 +0000

    hungry-distance: add 0.1.16 bottle.

--- Formula/hungry-distance.rb
+++ Formula/hungry-distance.rb
@@ -4,6 +4,12 @@ class HungryDistance < Formula
   url "https://github.com/PurpleBooth/hungry-distance/archive/v0.1.16.tar.gz"
   sha256 "63766c39bf1b99e8964a5d2b1bea450be05a4c0e6c3bdc0cf8d4bbc46740fa56"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/hungry-distance-0.1.16"
+    sha256 cellar: :any_skip_relocation, big_sur:      "6328414270fde94f57f48338b1e92f6f19219e49115b9fd67256bc7f3c0963e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "84522d03b6966f0b68946b775ee703742c1a1d8ef6fb04d3685911773e016b92"
+  end
+
   depends_on "rust" => :build
 
   def install
