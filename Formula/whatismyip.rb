2f59bd40954fc0f0b1d6e6d0d833cb8fdd7d89d2
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-01-06 18:48:07 +0000

    whatismyip: add 0.9.31 bottle.

--- Formula/whatismyip.rb
+++ Formula/whatismyip.rb
@@ -4,6 +4,12 @@ class Whatismyip < Formula
   url "https://github.com/PurpleBooth/whatismyip/archive/v0.9.31.tar.gz"
   sha256 "ef5945e1dad49c96d18f6076d41f9335e69df4f0cd7b9772dcae9bfe12b74c01"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/whatismyip-0.9.31"
+    sha256 cellar: :any_skip_relocation, big_sur:      "c7f0e659de679733d84bac62c380af6347b58e64968720ce88a312848cfa1bac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8f663f6e726db9790277606273f2d86545e84462a119ed65b68a540aa38cf4c4"
+  end
+
   depends_on "rust" => :build
 
   def install
