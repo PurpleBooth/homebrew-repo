2068dafac835c90acec8c21779e3b989e913132a
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-01-06 02:31:38 +0000

    setup-a-keyring-for-sops: add 0.42.27 bottle.

--- Formula/setup-a-keyring-for-sops.rb
+++ Formula/setup-a-keyring-for-sops.rb
@@ -4,6 +4,12 @@ class SetupAKeyringForSops < Formula
   url "https://github.com/PurpleBooth/setup-a-keyring-for-sops/archive/refs/tags/v0.42.27.tar.gz"
   sha256 "5813f31be3d01fada43a4bf5c830375943e3bae8056124601b60dc1f8d491c49"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/setup-a-keyring-for-sops-0.42.27"
+    sha256 cellar: :any_skip_relocation, big_sur:      "5528c5e381218920903b486082f3de0a32f08ed459effadffb56839b855791b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d7810f066b3d1f626d752e821eff29c6e27dd126c032fc3a8ddb38fd43102c93"
+  end
+
   depends_on "rust" => :build
 
   def install
