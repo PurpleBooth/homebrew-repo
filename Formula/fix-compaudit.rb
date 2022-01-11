ea9c2184592798a5e0632b372032e2068bd7a37f
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-01-11 14:20:10 +0000

    fix-compaudit: add 0.46.71 bottle.

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -4,6 +4,12 @@ class FixCompaudit < Formula
   url "https://github.com/PurpleBooth/fix-compaudit/archive/v0.46.71.tar.gz"
   sha256 "d071d3e01c2c724ffe499be0ec663255112856dd820e1cdff9068d2bcea73d61"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/fix-compaudit-0.46.71"
+    sha256 cellar: :any_skip_relocation, big_sur:      "f5ecf229e7d0d32323e7c62d47b497ab9984349abff63edac97d3073435e11e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e696e9a6ebb2bc70adef585ba3024c8c7b087598e29f6a6bba30b872e23b9a4c"
+  end
+
   depends_on "rust" => :build
   depends_on "zsh"
 
