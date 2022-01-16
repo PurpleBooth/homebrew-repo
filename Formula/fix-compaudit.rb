1a5d06fa0a5e9a71cc7bca5fd9759f7090c97514
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-01-16 10:54:25 +0000

    fix: Update fix-compaudit to v0.46.72
    Closes #1297.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/fix-compaudit.rb
+++ Formula/fix-compaudit.rb
@@ -1,14 +1,8 @@
 class FixCompaudit < Formula
   desc "Fixes problems reported by compuaudit"
   homepage "https://github.com/PurpleBooth/fix-compaudit"
-  url "https://github.com/PurpleBooth/fix-compaudit/archive/v0.46.71.tar.gz"
-  sha256 "d071d3e01c2c724ffe499be0ec663255112856dd820e1cdff9068d2bcea73d61"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/fix-compaudit-0.46.71"
-    sha256 cellar: :any_skip_relocation, big_sur:      "f5ecf229e7d0d32323e7c62d47b497ab9984349abff63edac97d3073435e11e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e696e9a6ebb2bc70adef585ba3024c8c7b087598e29f6a6bba30b872e23b9a4c"
-  end
+  url "https://github.com/PurpleBooth/fix-compaudit/archive/v0.46.72.tar.gz"
+  sha256 "69453b400f73172735755f7be1edf6750cc33a9846ebc61a6c018fdbc415a1d3"
 
   depends_on "rust" => :build
   depends_on "zsh"
