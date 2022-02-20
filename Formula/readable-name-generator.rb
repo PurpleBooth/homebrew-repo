994d9968c7f4b286d59062239d4bd7dc4b6af4e4
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-02-20 10:19:36 +0000

    fix: Update readable-name-generator to v2.100.11

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -1,20 +1,27 @@
 class ReadableNameGenerator < Formula
-  desc "Make conventional commits, faster, and consistently name scopes"
+  desc "Generate a readable names suitable for infrastructure"
   homepage "https://github.com/PurpleBooth/readable-name-generator"
-  url "https://github.com/PurpleBooth/readable-name-generator/archive/v2.100.10.tar.gz"
-  sha256 "20de72ae94f8a5b409e29109309393f65db3895e3c1da983edadd54634ace68c"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/readable-name-generator-2.100.10"
-    sha256 cellar: :any_skip_relocation, big_sur:      "bc96ba6b57d1a7f14f36fd4420d3c08fdb70d99bace4ec874d57e14d6bd10d88"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "269aaadc9ae3ef2d9087f407f6cb0f24d7cfceadf2afd69edbd034be4d3905f1"
-  end
+  url "https://github.com/PurpleBooth/readable-name-generator/archive/v2.100.11.tar.gz"
+  sha256 "f10a48816945bd0b7f5b41fe865290b236e4b85ff2a03e3502e3e39f0e06eaa2"
   depends_on "help2man" => :build
   depends_on "rust" => :build
   depends_on "specdown/repo/specdown" => :test
 
   def install
+    # Build binary
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+
+    # Completions
+    output = Utils.safe_popen_read("#{bin}/readable-name-generator", "--completion", "bash")
+    (bash_completion/binary.to_s).write output
+    output = Utils.safe_popen_read("#{bin}/readable-name-generator", "--completion", "zsh")
+    (zsh_completion/binary.to_s).write output
+    output = Utils.safe_popen_read("#{bin}/readable-name-generator", "--completion", "fish")
+    (fish_completion/binary.to_s).write output
+
+    # Man pages
+    output = Utils.safe_popen_read("help2man", "#{bin}/readable-name-generator")
+    (man1/"readable-name-generator.1").write output
   end
 
   test do
