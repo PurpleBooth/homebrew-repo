19da8591d05d69b1e7d1cfa6f9843837055a6bf9
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-02-23 19:45:39 +0000

    readable-name-generator 2.100.15
    fix: Update readable-name-generator to v2.100.15
    
    Closes #1379.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -1,14 +1,8 @@
 class ReadableNameGenerator < Formula
   desc "Generate a readable names suitable for infrastructure"
   homepage "https://github.com/PurpleBooth/readable-name-generator"
-  url "https://github.com/PurpleBooth/readable-name-generator/archive/v2.100.14.tar.gz"
-  sha256 "6adc5e5598fe358666c782673859a6cce2c1942736ba8f9fb750b7560e72220f"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/readable-name-generator-2.100.14"
-    sha256 cellar: :any_skip_relocation, big_sur:      "1e48da84421714dab540e6603c267c31fdcb429ff8dbc02fb01ba1d01fdf1fcd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6480effa95e31d7245a0235f5915b42198dc3771b129066fbbc1b3d00c0afa05"
-  end
+  url "https://github.com/PurpleBooth/readable-name-generator/archive/v2.100.15.tar.gz"
+  sha256 "d7d478b7c8ebf3036ad35f05f0165dce1b534e383d335e9030ce5b22a71eb6e5"
   depends_on "help2man" => :build
   depends_on "rust" => :build
   depends_on "specdown/repo/specdown" => :test
