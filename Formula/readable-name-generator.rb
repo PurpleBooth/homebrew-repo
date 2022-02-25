908eb91a35030c1e83318ab1d9e2c2becc732deb
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-02-25 19:29:44 +0000

    readable-name-generator: add 2.100.16 bottle.

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -3,6 +3,12 @@ class ReadableNameGenerator < Formula
   homepage "https://github.com/PurpleBooth/readable-name-generator"
   url "https://github.com/PurpleBooth/readable-name-generator/archive/v2.100.16.tar.gz"
   sha256 "dd00a00494f6de1801868a172b5aebd77b4de7cbe5aef93b77adc3b79b707538"
+
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/readable-name-generator-2.100.16"
+    sha256 cellar: :any_skip_relocation, big_sur:      "ddc258e3e97d38fe65f301a6292ad1e8656ab64a3882edea21b4d26f74e1be2d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "df326d6382730fa470332ccdb6f3d10e0b1de48217f1a69a610e1bb4bedeaf3c"
+  end
   depends_on "help2man" => :build
   depends_on "rust" => :build
   depends_on "specdown/repo/specdown" => :test
