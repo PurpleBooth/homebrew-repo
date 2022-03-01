8f719886114c2478190db885ee97d4892050262f
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-03-01 08:34:07 +0000

    fix-gnupg-permissions: add 0.49.42 bottle.

--- Formula/fix-gnupg-permissions.rb
+++ Formula/fix-gnupg-permissions.rb
@@ -5,6 +5,12 @@ class FixGnupgPermissions < Formula
   sha256 "945b3a40c683ec6bf639ed4bf53a238112d4dfb61b6587066cdf412ed793e281"
   license "CC0-1.0"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/fix-gnupg-permissions-0.49.42"
+    sha256 cellar: :any_skip_relocation, big_sur:      "4d6c85ba47c2cd2beffc9962a4adab36ba34989813e9f42f9663a03646a6d029"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "29afc3c634a87b50210f64cf8147fde9882fbea92626925311db3a543f29ab3d"
+  end
+
   depends_on "rust" => :build
 
   def install
