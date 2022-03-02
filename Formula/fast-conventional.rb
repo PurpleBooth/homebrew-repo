93b08adcefc4d7990bf3a22b2677d34bb66a913c
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-03-02 17:51:19 +0000

    fast-conventional 1.0.17
    fix: Update fast-conventional to v1.0.17
    
    Closes #1403.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/fast-conventional.rb
+++ Formula/fast-conventional.rb
@@ -1,14 +1,8 @@
 class FastConventional < Formula
   desc "Make conventional commits, faster, and consistently name scopes"
   homepage "https://github.com/PurpleBooth/fast-conventional"
-  url "https://github.com/PurpleBooth/fast-conventional/archive/v1.0.16.tar.gz"
-  sha256 "8c8fe2883bb5a297c3950646a28f59ffa846489ac60c8cd7912773fd7384ffa7"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/fast-conventional-1.0.16"
-    sha256 cellar: :any_skip_relocation, big_sur:      "0e1ea74318b3f2fcd1979009a8ffdba98fd6e7ae427772cc11dff7f23e2d175b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3107a8a3697d845404e472290ba210a476712471963ef02b2aa699cf1265d11a"
-  end
+  url "https://github.com/PurpleBooth/fast-conventional/archive/v1.0.17.tar.gz"
+  sha256 "d61895405484678efacbfebd9819cea35944b83ca4930bde0a7ca4c1aebdd1c0"
 
   depends_on "rust" => :build
   depends_on "socat" => :test
