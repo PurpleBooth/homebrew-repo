21f3ce0ba28078f709b051b968f400513c952b25
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-03-06 07:37:37 +0000

    fast-conventional: add 1.1.0 bottle.

--- Formula/fast-conventional.rb
+++ Formula/fast-conventional.rb
@@ -4,6 +4,12 @@ class FastConventional < Formula
   url "https://github.com/PurpleBooth/fast-conventional/archive/v1.1.0.tar.gz"
   sha256 "764b30c14047094970d0bd9bb87d8eeb588efe2e8dac81d3eec33fa69d0ff199"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/fast-conventional-1.1.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "24af0f3eb0e035dc3dfa7a652cbf6d6c190918aeec34e62f249e28651d072e46"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5514ddf303e46afde7d1ac5d3445c1feed7330ced16113f6e0ff5710193a970e"
+  end
+
   depends_on "rust" => :build
   depends_on "socat" => :test
   depends_on "specdown/repo/specdown" => :test
