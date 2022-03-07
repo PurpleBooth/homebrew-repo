09b8efa49297b5a511092368e8a3147575378df8
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-03-07 11:29:25 +0000

    git-mit 5.12.41
    fix: Update git-mit to v5.12.41
    
    Closes #1423.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,14 +1,8 @@
 class GitMit < Formula
   desc "Minimalist set of hooks to aid pairing and link commits to issues"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/v5.12.40.tar.gz"
-  sha256 "fe2b900ddf472436ebb3bed008a97a0c1770687b8ef2d15f74512211d7465921"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-mit-5.12.40"
-    sha256 cellar: :any,                 big_sur:      "fa9b80e750cb2625b6e2c69c5784278dcaa019253adcbc4240c6c1e5b908b2e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "06540ad5ee9e6983593571f03d0c423068fd07a1ed2b42e115c4ffd5b01df31d"
-  end
+  url "https://github.com/PurpleBooth/git-mit/archive/v5.12.41.tar.gz"
+  sha256 "44ba3436bfb89e040f40b4c8d305e0362ebdbe74103664b55fddd7209c5f1444"
   depends_on "help2man" => :build
   depends_on "rust" => :build
   depends_on "openssl@1.1"
