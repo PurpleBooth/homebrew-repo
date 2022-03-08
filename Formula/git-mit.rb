0cb324389b56b34985e29c50047196ddbe0b4fd8
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-03-08 12:01:07 +0000

    git-mit: add 5.12.42 bottle.

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -3,6 +3,12 @@ class GitMit < Formula
   homepage "https://github.com/PurpleBooth/git-mit"
   url "https://github.com/PurpleBooth/git-mit/archive/v5.12.42.tar.gz"
   sha256 "9669c67dc346d2726390c9ccf0c71fd2289ed1be31f06e855df9ac43e97111b7"
+
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-mit-5.12.42"
+    sha256 cellar: :any,                 big_sur:      "e4c8a83f2cd33d2d35525b8ffac3861c6d4dd22cf1537f294438aae07c77b8dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6a2d2de15faaf17858a9b15bef6090510a03b9ac0be2432dc69aae5cc486704f"
+  end
   depends_on "help2man" => :build
   depends_on "rust" => :build
   depends_on "openssl@1.1"
