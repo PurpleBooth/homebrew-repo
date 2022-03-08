65bfc1ca0fbee7681ad172e16c302286e2b3388d
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-03-08 00:40:20 +0000

    game-of-life: add 1.53.57 bottle.

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -4,6 +4,12 @@ class GameOfLife < Formula
   url "https://github.com/PurpleBooth/game-of-life/archive/v1.53.57.tar.gz"
   sha256 "e3be4ae0d7deaf36f5b3230a8fbbd2acf662d5700f2cb0e23e6981a1d0b3c718"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/game-of-life-1.53.57"
+    sha256 cellar: :any_skip_relocation, big_sur:      "2debebf8c5dbbd5c09251861a24d9682446e11ad6fb603c17e84d742cb313df1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c10926fb3ed6aea2cfeca9092158f5b3704f07796e207118b2fd51fadf0955fb"
+  end
+
   depends_on "rust" => :build
 
   def install
