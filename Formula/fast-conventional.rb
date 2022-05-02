860855eee1d9d412d33f5140231dc24a78f265e4
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-05-02 17:43:34 +0000

    fast-conventional 2.2.6
    fix: Update fast-conventional to v2.2.6
    
    Closes #1483.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/fast-conventional.rb
+++ Formula/fast-conventional.rb
@@ -1,14 +1,8 @@
 class FastConventional < Formula
   desc "Make conventional commits, faster, and consistently name scopes"
   homepage "https://github.com/PurpleBooth/fast-conventional"
-  url "https://github.com/PurpleBooth/fast-conventional/archive/v2.2.5.tar.gz"
-  sha256 "d65a0532ff0c254728d4c4857ba7b6ba4972ffce8b05e99338e6313c72dc5777"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/fast-conventional-2.2.5"
-    sha256 cellar: :any_skip_relocation, big_sur:      "f5a9291ecc2a29371ae19ec98b3ecd8a5335786a30bf55e2b20dac8ba5792804"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2fbb82189a2f25c27ed9553309690c62fb65315448537bc1edf9c205c1545351"
-  end
+  url "https://github.com/PurpleBooth/fast-conventional/archive/v2.2.6.tar.gz"
+  sha256 "b224a28a831c33463343d8b5e25b681f97aaede6142918b7786117247245a317"
 
   depends_on "help2man" => :build
   depends_on "rust" => :build
