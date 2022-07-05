77dcc96411234a9cf169e4098a4a73f9f0544f32
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-07-05 21:52:59 +0000

    fix: Update ellipsis to v0.6.49

--- Formula/ellipsis.rb
+++ Formula/ellipsis.rb
@@ -1,14 +1,8 @@
 class Ellipsis < Formula
   desc "Manage and provision dotfiles"
   homepage "https://github.com/PurpleBooth/ellipsis"
-  url "https://github.com/PurpleBooth/ellipsis/archive/v0.6.48.tar.gz"
-  sha256 "6ddf62900cbde9164f26c23a0ebf63538ceb065fb287e79a85825ce9bbb43145"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/ellipsis-0.6.48"
-    sha256 cellar: :any_skip_relocation, big_sur:      "37fc591c2a0bae41ac50de9e72bda0defb39492e011a2cacd5d1ab8a416d9a7a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ba00498c69371fea9d154f30c36045d63d9d467ad05e6bfb36ea6fa0201f6ccf"
-  end
+  url "https://github.com/PurpleBooth/ellipsis/archive/v0.6.49.tar.gz"
+  sha256 "7c229970b325a6a6deb08a11b23bc458a7fef413edabf2a44b5ceaeda359bb59"
 
   depends_on "rust" => :build
 
