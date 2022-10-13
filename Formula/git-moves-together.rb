40086f266823b584cf18c3a19c11204de6308ae5
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-10-13 11:04:04 +0000

    git-moves-together: add 2.5.40 bottle.

--- Formula/git-moves-together.rb
+++ Formula/git-moves-together.rb
@@ -4,6 +4,12 @@ class GitMovesTogether < Formula
   url "https://github.com/PurpleBooth/git-moves-together/archive/v2.5.40.tar.gz"
   sha256 "03528e38095dda8cf609c6a0ac07808964138ac5af7751b257c41c1a9f67b5b7"
 
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-moves-together-2.5.40"
+    sha256 cellar: :any,                 big_sur:      "7b76beeabbe56d3f90e6d57bccb33b95cfaa5410a9d95d78077179a5586eb8ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "120e619654cf88018f53144f2beab1372ca95443913ab9a4c0b38139e68d9156"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
   on_linux do
