27eb96c2a4485dcac266edbecd1218c05f47968d
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-10-13 18:22:53 +0000

    fix: Update fast-conventional to v2.3.3

--- Formula/fast-conventional.rb
+++ Formula/fast-conventional.rb
@@ -1,8 +1,8 @@
 class FastConventional < Formula
   desc "Make conventional commits, faster, and consistently name scopes"
   homepage "https://github.com/PurpleBooth/fast-conventional"
-  url "https://github.com/PurpleBooth/fast-conventional/archive/v2.3.2.tar.gz"
-  sha256 "765a766f84b63b1ac3b706e96b998a0fe93bfe7b06b9838091b55cb133eed5cb"
+  url "https://github.com/PurpleBooth/fast-conventional/archive/v2.3.3.tar.gz"
+  sha256 "6db9de1a241f0aae4303b8467bb8a3217cd84721d4b03ed209004a8ef5b6f263"
 
   depends_on "help2man" => :build
   depends_on "rust" => :build
