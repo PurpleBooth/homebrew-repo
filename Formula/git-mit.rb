04b9c69140d0dc489f915cba0198f9b125d89f93
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2022-11-07 12:23:09 +0000

    git-mit: add 5.12.107 bottle.

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -3,6 +3,12 @@ class GitMit < Formula
   homepage "https://github.com/PurpleBooth/git-mit"
   url "https://github.com/PurpleBooth/git-mit/archive/v5.12.107.tar.gz"
   sha256 "4f0a5c07406f50e4212646ae2507aa12f45c1212af39cf438e23abae594b111f"
+
+  bottle do
+    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-mit-5.12.107"
+    sha256 cellar: :any,                 monterey:     "12477f297c2a87b819bdb2b35d8884ac580ecd7a869e558f7d39507d42cd6628"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0c6a77656ce0e6ac7e129f44544a62e819b5ec21290b16bc27df8a9e9b87765b"
+  end
   depends_on "help2man" => :build
   depends_on "rust" => :build
   depends_on "openssl@1.1"
