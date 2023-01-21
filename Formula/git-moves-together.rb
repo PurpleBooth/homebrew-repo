2510e616c6554296529efc50cb46e71434aea848
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2023-01-21 00:03:58 +0000

    git-moves-together 2.5.50
    fix: Update git-moves-together to v2.5.50
    
    Closes #1614.
    
    Signed-off-by: Billie Thompson <133327+PurpleBooth@users.noreply.github.com>

--- Formula/git-moves-together.rb
+++ Formula/git-moves-together.rb
@@ -1,14 +1,8 @@
 class GitMovesTogether < Formula
   desc "Find coupling in git repositories"
   homepage "https://github.com/PurpleBooth/git-moves-together"
-  url "https://github.com/PurpleBooth/git-moves-together/archive/v2.5.46.tar.gz"
-  sha256 "3c0f61ea909ef258f6e90ba37def272415e3d7c4d3e61b50966ce08deb83cf90"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-moves-together-2.5.46"
-    sha256 cellar: :any,                 monterey:     "6296e2fec5da27e0cc19e27fd29caf2fd566490e0e01ae2327e58b6dceb884a9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ab42ba1de1d905adc3b06cf4606c2b5a97143381d59e0abf8e8d1ac2855bef46"
-  end
+  url "https://github.com/PurpleBooth/git-moves-together/archive/v2.5.50.tar.gz"
+  sha256 "5ed627aaae6c619aec5131e6d054dbd674d18b0663a5aef52c620ab465c14234"
 
   depends_on "rust" => :build
   depends_on "openssl@1.1"
