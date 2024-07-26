d0209398dee827c3c4613e32d96e17405abd49bb
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2024-07-26 18:52:22 +0200

    refactor: Remove unused directory

--- bin/install-dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -xeuo pipefail
-
-brew cask install google-cloud-sdk
--- bin/tap-without-clone.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -xeuo pipefail
-
-mkdir -p "$(brew --repo)/Library/Taps/purplebooth"
-cp -r ./. "$(brew --repo)/Library/Taps/purplebooth/homebrew-repo"
