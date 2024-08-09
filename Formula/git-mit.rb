7559d7257ae2ce834824dee54464b34341605dba
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2024-08-09 15:25:30 +0000

    fix: Update git-mit to v5.13.11

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,14 +1,8 @@
 class GitMit < Formula
   desc "Minimalist set of hooks to aid pairing and link commits to issues"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v5.13.7.tar.gz"
-  sha256 "5b62c67fcf7085622f17a5adb66dac58ff8bb3c8397818adce22e2deda63ee03"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-mit-5.13.7"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "96b6ee0aa3e484843c217b37d2b4c360320d15bdb8a71afbffaea708d2d4212c"
-  end
+  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v5.13.11.tar.gz"
+  sha256 "d34efc9b08d0735f8a4b779720bd6e50bd0ebd0b12d69feed4a6f91aff2fd8aa"
   depends_on "help2man" => :build
   depends_on "homebrew/core/rust" => :build
   depends_on "openssl@1.1"
