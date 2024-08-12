901729b362051405e640a9eb723b487709451cff
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2024-08-12 08:51:12 +0000

    fix: Update git-mit to v5.13.13
    Closes #1784.
    
    Signed-off-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- Formula/git-mit.rb
+++ Formula/git-mit.rb
@@ -1,15 +1,8 @@
 class GitMit < Formula
   desc "Minimalist set of hooks to aid pairing and link commits to issues"
   homepage "https://github.com/PurpleBooth/git-mit"
-  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v5.13.12.tar.gz"
-  sha256 "1904c73b705a44e0436149b6c40af8f92f248f3ac3266234baeb352b97c4407c"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-mit-5.13.12"
-    sha256 cellar: :any,                 arm64_sonoma: "9bd3257650582881bd35308a18182be3adc20ac0cd0db238c6bdff16271d0819"
-    sha256 cellar: :any,                 ventura:      "82a648fbdfd7f06a0524ce9272a0f057a0188af3c36eb628a648835ab7b085e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3a90d18fbb803ba34d96e8b3a6d1ba84451215961c972badfe18af764fc73c9d"
-  end
+  url "https://github.com/PurpleBooth/git-mit/archive/refs/tags/v5.13.13.tar.gz"
+  sha256 "11b2a6f7080f8983aa42b179014872b3baeddcc2804260c02b942966bd641a2b"
   depends_on "help2man" => :build
   depends_on "homebrew/core/rust" => :build
   depends_on "openssl@3"
