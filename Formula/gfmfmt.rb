046f3ef48d4918edbcfa5573400514a19514904b
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2024-08-14 12:34:33 +0000

    fix: Update gfmfmt to v1.0.1
    Closes #1790.
    
    Signed-off-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- Formula/gfmfmt.rb
+++ Formula/gfmfmt.rb
@@ -1,14 +1,8 @@
 class Gfmfmt < Formula
   desc "Format GitHub flavour markdown"
   homepage "https://github.com/PurpleBooth/gfmfmt"
-  url "https://github.com/PurpleBooth/gfmfmt/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "ab367882ebc0ad72e68790aca5922ea573cd84bfc391b6d40fb74a9baa25ac38"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/gfmfmt-0.1.2"
-    sha256 cellar: :any_skip_relocation, catalina:     "f129f7a7aee9e8a745c3e216ab02cb493db0395157825846250b409388db57c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6885db47e1cf69c700e9ee16b5e40f4cffc159eab51ed85ab22eb4664f568942"
-  end
+  url "https://github.com/PurpleBooth/gfmfmt/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "6b8b75b13218ff4b43323d90f4c6d904cf11b086d1cb4b3d011cab0e138e2487"
   depends_on "bash"
   depends_on "pandoc"
 
