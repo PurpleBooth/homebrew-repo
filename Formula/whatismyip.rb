04af87cb0f8eda4dc774decbeda009c110310810
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2024-08-24 02:12:30 +0000

    fix: Update whatismyip to v0.12.1
    Closes #1818.
    
    Signed-off-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- Formula/whatismyip.rb
+++ Formula/whatismyip.rb
@@ -1,15 +1,8 @@
 class Whatismyip < Formula
   desc "Work out what your IP is"
   homepage "https://github.com/PurpleBooth/whatismyip"
-  url "https://github.com/PurpleBooth/whatismyip/archive/refs/tags/v0.12.0.tar.gz"
-  sha256 "29811ca4568c23653d3da23d67d1115748161f76859a464dccaea36ba491c6a2"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/whatismyip-0.12.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "b6cf1ff4455a2ca708acbbe0911ad26b3b8edac8fb26fff7a86dc47ea7157c87"
-    sha256 cellar: :any_skip_relocation, ventura:      "92e6c515744ec5eb74fb587c70ef9bcac51b30e5b42875bd4d90696a215e194e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "70125c0b061d3a5ac0eae7d19d43c23620b3e8821c9425e7a0e6a834b1779a43"
-  end
+  url "https://github.com/PurpleBooth/whatismyip/archive/refs/tags/v0.12.1.tar.gz"
+  sha256 "c2c13e0b1c4e14944423b572d29cd21288a51b2b2f32396c72683cd61e2e5c2e"
 
   depends_on "rust" => :build
 
