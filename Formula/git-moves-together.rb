88b9351129c05c65008da96cc813719d865fb96f
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2024-08-31 07:42:23 +0000

    fix: Update git-moves-together to v2.6.3

--- Formula/git-moves-together.rb
+++ Formula/git-moves-together.rb
@@ -1,15 +1,8 @@
 class GitMovesTogether < Formula
   desc "Find coupling in git repositories"
   homepage "https://github.com/PurpleBooth/git-moves-together"
-  url "https://github.com/PurpleBooth/git-moves-together/archive/refs/tags/v2.6.2.tar.gz"
-  sha256 "ed14e31a0cab914e755f1e384d1d98704edbca27999472619654679f1fde7f0a"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/git-moves-together-2.6.2"
-    sha256 cellar: :any,                 arm64_sonoma: "cfe76efa6540faa78a18c42d856122cc78b19ad7a44766f1df9b67947898d1d0"
-    sha256 cellar: :any,                 ventura:      "be70597eb83e0140925febac58fe61091b5a3f9a6e9354fa8723aa41dd955ad1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "09bfeed5d3e94eb108f76e1e6c60c3edfe5605690e338528a36f7cfbcdb54244"
-  end
+  url "https://github.com/PurpleBooth/git-moves-together/archive/refs/tags/v2.6.3.tar.gz"
+  sha256 "98880f26ebd8380289afd2d543c53c2c9b300bba979561c825f5235a503394ae"
 
   depends_on "rust" => :build
   depends_on "openssl@3"
