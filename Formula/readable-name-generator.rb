a28b928e0f40aff05c2f83f3a0b967b233ce1dd6
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2024-09-03 10:25:47 +0000

    fix: Update readable-name-generator to v4.1.9
    Closes #1847.
    
    Signed-off-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -1,15 +1,8 @@
 class ReadableNameGenerator < Formula
   desc "Generate a readable names suitable for infrastructure"
   homepage "https://github.com/PurpleBooth/readable-name-generator"
-  url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v4.1.7.tar.gz"
-  sha256 "95b098e1981df7904d7c2df1158c410c62ef05512101bbb5ffb0cf81700bda33"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/readable-name-generator-4.1.7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "3fb4f5842f085eba09e742816f150bfbef1e37a6cb9320a6d620ed4408b823e4"
-    sha256 cellar: :any_skip_relocation, ventura:      "94b188bc50d45dd53e1a323c1b27d629b9c085789305bbd0fe66310c3e891d97"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5b3ee09d4839b0efe6c8919a24ad9f851d3a81925340e7440cadb84c1b929345"
-  end
+  url "https://github.com/PurpleBooth/readable-name-generator/archive/refs/tags/v4.1.9.tar.gz"
+  sha256 "6f8e7709f7437ab5e969d570d4acf540a728694e6951249f0ab92d9f0cad1825"
   depends_on "help2man" => :build
   depends_on "rust" => :build
 
