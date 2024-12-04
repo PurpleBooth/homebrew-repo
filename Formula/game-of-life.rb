76bfb93f24e42562a430d1b3d006380dd58b1040
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2024-12-04 16:53:55 +0000

    fix: Update game-of-life to v1.53.80

---
+++ Formula/game-of-life.rb
@@ -0,0 +1,18 @@
+class GameOfLife < Formula
+  desc "PurpleBooth's implementation of Conway's Game of life"
+  homepage "https://github.com/PurpleBooth/game-of-life"
+  url "https://github.com/PurpleBooth/game-of-life/archive/refs/tags/v1.53.80.tar.gz"
+  sha256 "00ccf2a34353c56722336d34211031f6373f86e02e3d3d0ab2e38667c305940f"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/game-of-life", "-h"
+    system "#{bin}/game-of-life", "-V"
+    system "#{bin}/game-of-life", "-s", "1"
+  end
+end
