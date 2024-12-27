b5e73c3e2c29b9fb86500c76ec900d2fba4931c5
SolaceRenovateFox <SolaceRenovateFox@noreply.codeberg.org>
2024-12-27 23:11:51 +0000

    fix: upgrade fast-conventional to 2.3.65

--- Formula/fast-conventional.rb
+++ Formula/fast-conventional.rb
@@ -1,8 +1,9 @@
 class FastConventional < Formula
   desc "Make conventional commits, faster, and consistently name scopes"
-  homepage "https://github.com/PurpleBooth/fast-conventional"
-  url "https://github.com/PurpleBooth/fast-conventional/archive/refs/tags/v2.3.6.tar.gz"
-  sha256 "9501c226e9e20d9704197c54dc14afe3a0757e20b3a066f0e7b1fd96f87062fb"
+  homepage "https://codeberg.org/PurpleBooth/fast-conventional"
+  url "https://codeberg.org/PurpleBooth/fast-conventional/archive/main.tar.gz"
+  version "2.3.65"
+  sha256 "fb1bffff68a5d8bfe78897a050c8e0d5156dbaf713f67f947cb2faa144179d5f"
 
   depends_on "help2man" => :build
   depends_on "rust" => :build
