3eca8ba0da40919be0a72b2a6a84237511f00a98
SolaceRenovateFox <SolaceRenovateFox@noreply.codeberg.org>
2025-02-24 23:51:33 +0000

    fix: upgrade readable-name-generator to 4.1.37

--- Formula/readable-name-generator.rb
+++ Formula/readable-name-generator.rb
@@ -2,15 +2,8 @@ class ReadableNameGenerator < Formula
   desc "Generate a readable names suitable for infrastructure"
   homepage "https://codeberg.org/PurpleBooth/readable-name-generator"
   url "https://codeberg.org/PurpleBooth/readable-name-generator/archive/main.tar.gz"
-  version "4.1.36"
-  sha256 "fae04f4f0db9394e12193f777e2f4f92fa7eadfb543b1167ba0108e2124709d9"
-
-  bottle do
-    root_url "https://github.com/PurpleBooth/homebrew-repo/releases/download/readable-name-generator-4.1.36"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2e1e09160cbab1487c9dcf0ed7a780b557dc569c5af8a480d8083bc9bd1a3d9d"
-    sha256 cellar: :any_skip_relocation, ventura:       "d570aa12894dbd2d3971b21ccdcdb90d1e9b924a8106563711ec7aa6bf5ffe9d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "11c6a1e4330aa1f3909e1837efe114f2aff71aa04f1ee82ee027add6f46b87ad"
-  end
+  version "4.1.37"
+  sha256 "94d16b47f14e946fae37b763b642f91b15a09fa53c4539b77cafc8d1b24a6178"
   depends_on "help2man" => :build
   depends_on "rust" => :build
 
