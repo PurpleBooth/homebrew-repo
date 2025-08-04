ae467c450d640d365b7bfa22029651ce828ddff0
SolaceRenovateFox <SolaceRenovateFox@noreply.codeberg.org>
2025-08-04 08:11:16 +0000

    fix: upgrade whatismyip to 0.15.4

--- Formula/whatismyip.rb
+++ Formula/whatismyip.rb
@@ -1,8 +1,8 @@
 class Whatismyip < Formula
   desc "Work out what your IP is"
   homepage "https://codeberg.org/PurpleBooth/whatismyip"
-  url "https://codeberg.org/PurpleBooth/whatismyip/archive/v0.15.0.tar.gz"
-  sha256 "ed03c739f2b71f0537f1b58e07d8d3013f7b052d96636746f0f674ea572273db"
+  url "https://codeberg.org/PurpleBooth/whatismyip/archive/v0.15.3.tar.gz"
+  sha256 "bcf71700882fc41e84e7e26d895f653f2bf8e110f3558a0757d11a2cf4e4a24a"
   depends_on "rust" => :build
 
   def install
